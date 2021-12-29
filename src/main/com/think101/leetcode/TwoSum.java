77737e3a5aec6b7e40db3a56415308e8967e15a4
think101 <taohuayuan2000@hotmail.com>
2021-12-29 00:41:13 -0800

    TwoSum

---
+++ src/main/com/think101/leetcode/TwoSum.java
@@ -0,0 +1,29 @@
+package main.com.think101.leetcode;
+
+import java.util.Arrays;
+
+public class TwoSum {
+    public static void main(String[] args) {
+        int[] nums = {2, 7, 11, 15};
+        int target = 9;
+
+        Solution solution = new Solution();
+        System.out.println(Arrays.toString(solution.twoSum(nums, target)));
+    }
+}
+
+class Solution {
+    public int[] twoSum(int[] nums, int target) {
+
+        for(int i = 0; i < nums.length; i++){
+            for(int j = i + 1; j < nums.length; j++){
+                if(nums[i] + nums[j] == target){
+                    return new int[]{i, j};
+                }
+            }
+        }
+
+        return null;
+    }
+}
+
