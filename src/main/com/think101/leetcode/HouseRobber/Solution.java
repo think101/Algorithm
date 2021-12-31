0711438222908eb9b7b71506b27a2b610186efcf
think101 <taohuayuan2000@hotmail.com>
2021-12-31 14:50:58 -0800

    198 House Robber improvement

--- src/main/com/think101/leetcode/HouseRobber/Solution.java
+++ src/main/com/think101/leetcode/HouseRobber/Solution.java
@@ -8,6 +8,21 @@ public class Solution {
 
         int[] result = new int[nums.length];
         result[0] = nums[0];
+        result[1] = Math.max(nums[0], nums[1]);
+
+        for(int i=2; i<nums.length; i++) {
+            result[i] = Math.max(result[i-2]+nums[i], result[i-1]);
+        }
+
+        return result[nums.length-1];
+    }
+
+    public int rob_v1(int[] nums) {
+        if(nums.length==1) return nums[0];
+        if(nums.length==2) return Math.max(nums[0], nums[1]);
+
+        int[] result = new int[nums.length];
+        result[0] = nums[0];
         result[1] = nums[1];
 
         for(int i=2; i<nums.length; i++) {
