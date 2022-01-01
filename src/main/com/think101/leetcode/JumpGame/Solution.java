b2946330c667362e4c18985440d14eccd2299c55
think101 <taohuayuan2000@hotmail.com>
2022-01-01 14:21:49 -0800

    55 Jump Game

---
+++ src/main/com/think101/leetcode/JumpGame/Solution.java
@@ -0,0 +1,15 @@
+package main.com.think101.leetcode.JumpGame;
+
+public class Solution {
+    public boolean canJump(int[] nums) {
+        int[] furthestIndex = new int[nums.length];
+        furthestIndex[0] = nums[0];
+
+        for(int i=1; i<nums.length; i++) {
+            if(nums[i-1]==0 && furthestIndex[i-1] < i) return false;
+            furthestIndex[i] = Math.max(furthestIndex[i-1], nums[i] + i);
+        }
+
+        return true;
+    }
+}
