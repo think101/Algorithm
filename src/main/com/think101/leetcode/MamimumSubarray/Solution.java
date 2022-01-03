956bb7e78257ca2aab041708b10244e61c7b4107
think101 <taohuayuan2000@hotmail.com>
2022-01-03 10:25:47 -0800

    53 Mamimum Subarray enhancement, recursion and DP solution

--- src/main/com/think101/leetcode/MamimumSubarray/Solution.java
+++ src/main/com/think101/leetcode/MamimumSubarray/Solution.java
@@ -1,7 +1,51 @@
 package main.com.think101.leetcode.MamimumSubarray;
 
 public class Solution {
-    public int maxSubArray(int[] nums) {
+
+    /*
+     * DP, based on V2
+     */
+    public int maxSubArrayV3(int[] nums) {
+        int[] dp = new int[nums.length];
+        dp[0] = nums[0];
+
+        int res = dp[0];
+        for(int i=1; i<nums.length; i++){
+            dp[i] = Math.max(nums[i], dp[i-1]+nums[i]);
+            if(dp[i]>res)
+                res = dp[i];
+        }
+
+        return res;
+
+    }
+
+    /*
+     * dp[i] is the max sub array sum ends with index i
+     *
+     * recursion method, Time Limit Exceeded
+     */
+    public int maxSubArrayV2(int[] nums) {
+        int[] dp = new int[nums.length];
+
+        for(int i=0; i<nums.length; i++)
+            dp[i] = dp(i, nums);
+
+        int res = dp[0];
+        for(int i=0; i<nums.length; i++)
+            if(dp[i]>res)
+                res = dp[i];
+
+        return res;
+
+    }
+
+    private int dp(int i, int[] nums) {
+        if(i==0) return nums[i];
+        return Math.max(nums[i]+dp(i-1, nums), nums[i]);
+    }
+
+    public int maxSubArrayV1(int[] nums) {
         int sum = nums[0];
         int prevSum = nums[0];
 
