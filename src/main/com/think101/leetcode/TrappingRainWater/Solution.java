361ccd0578cabf4ea0eb56ecc9affa036ebf4ed9
think101 <taohuayuan2000@hotmail.com>
2022-01-11 23:58:45 -0800

    42. Trapping Rain Water DP solution

--- src/main/com/think101/leetcode/TrappingRainWater/Solution.java
+++ src/main/com/think101/leetcode/TrappingRainWater/Solution.java
@@ -5,7 +5,30 @@ public class Solution {
     public static void main(String[] args) {
         int[] height = new int[]{0,1,0,2,1,0,1,3,2,1,2,1};
 
-        System.out.println(trap_wrong(height));
+        System.out.println(trap(height));
+    }
+
+    public static int trap(int[] height) {
+        int[] prevMax = new int[height.length];
+        int[] nextMax = new int[height.length];
+        prevMax[0] = height[0];
+        nextMax[height.length-1] = height[height.length-1];
+
+        for(int i=1; i<height.length; i++) {
+            prevMax[i] = Math.max(height[i], prevMax[i - 1]);
+        }
+
+        for(int i=height.length-2; i>=0; i--) {
+            nextMax[i] = Math.max(height[i], nextMax[i + 1]);
+        }
+
+        int res = 0;
+        for(int i=0; i<height.length; i++) {
+            int t = Math.min(prevMax[i], nextMax[i]);
+            res += t > height[i] ? t - height[i] : 0;
+        }
+
+        return res;
     }
 
     /*
