bc15d8fb7bb9d9a5af9acd9e2391aef9dc51d4ec
think101 <taohuayuan2000@hotmail.com>
2022-01-14 23:53:37 -0800

    120. Triangle

--- src/main/com/think101/leetcode/MinimumFallingPathSum/Solution.java
+++ src/main/com/think101/leetcode/MinimumFallingPathSum/Solution.java
@@ -1,41 +1,25 @@
 package main.com.think101.leetcode.MinimumFallingPathSum;
 
+import java.util.List;
+
 public class Solution {
 
-    public int minFallingPathSum(int[][] matrix) {
-        int row = matrix.length, col = matrix[0].length;
+    public int minimumTotal(List<List<Integer>> triangle) {
+        int row = triangle.size(), col = triangle.get(triangle.size()-1).size();
         int[][] dp = new int[row][col];
-        System.arraycopy(matrix[row - 1], 0, dp[row - 1], 0, col);
 
-        //for each line
-        for(int r = row-2; r >= 0; r-- ){
-            for(int c=0; c<col; c++) {
-                if(c-1>=0) {
-                    if(c+1<col) {
-                        dp[r][c] = matrix[r][c] +  Math.min(dp[r+1][c-1], Math.min(dp[r+1][c], dp[r+1][c+1]));
-                    }
-                    else {
-                        dp[r][c] = matrix[r][c] + Math.min(dp[r+1][c-1], dp[r+1][c]);
-                    }
-                }
-                else {
-                    if(c+1<col) {
-                        dp[r][c] = matrix[r][c] + Math.min(dp[r+1][c], dp[r+1][c+1]);
-                    }
-                    else {
-                        dp[r][c] = matrix[r][c];
-                    }
-                }
+        for(int i=0; i<col; i++) {
+            dp[row-1][i] = triangle.get(row-1).get(i);
+        }
+
+        // bottom up, each row
+        for(int i=row-2; i>=0; i--) {
+            for(int j=0; j<=i; j++) {
+                dp[i][j] = triangle.get(i).get(j) + Math.min(dp[i+1][j], dp[i+1][j+1]);
             }
         }
 
-        int res = Integer.MAX_VALUE;
-        for(int i=0; i<col; i++) {
-            if(dp[0][i] < res)
-                res = dp[0][i];
-        }
-
-        return res;
+        return dp[0][0];
     }
 
 }
