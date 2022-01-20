0507d3c1f58efe11a4c2cffb31c011d94da1efdb
think101 <taohuayuan2000@hotmail.com>
2022-01-19 19:34:32 -0800

    300. Longest Increasing Subsequence

---
+++ src/main/com/think101/leetcode/LongestIncreasingSubsequence/Solution.java
@@ -0,0 +1,29 @@
+package main.com.think101.leetcode.LongestIncreasingSubsequence;
+
+public class Solution {
+
+    public int lengthOfLIS(int[] nums) {
+        // dp[i] the length of the longest increasing subsequence end at i;
+        int[] dp = new int[nums.length];
+        dp[0] = 1;
+
+        for(int i=1; i<nums.length; i++) {
+            int t = 1;
+            dp[i] = 1;
+            for(int j=0; j<i; j++) {
+                if(nums[i] > nums[j] && dp[j]+1 > t) {
+                    t = dp[j]+1;
+                    dp[i] = t;
+                }
+            }
+        }
+
+        int res = 1;
+        for(int i=0; i<nums.length; i++) {
+            if(dp[i] > res)
+                res = dp[i];
+        }
+
+        return res;
+    }
+}
