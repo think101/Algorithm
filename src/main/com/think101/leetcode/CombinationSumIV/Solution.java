480ecd1a48365ad3a26c7df47d8e81dd7be61afc
think101 <taohuayuan2000@hotmail.com>
2022-01-22 10:47:21 -0800

    377. Combination Sum IV

---
+++ src/main/com/think101/leetcode/CombinationSumIV/Solution.java
@@ -0,0 +1,17 @@
+package main.com.think101.leetcode.CombinationSumIV;
+
+public class Solution {
+    public int combinationSum4(int[] nums, int target) {
+        int[] dp = new int[target+1];
+        dp[0] = 1;
+
+        for(int i=1; i<target+1; i++) {
+            for(int n : nums) {
+                if(i-n>=0)
+                    dp[i] += dp[i-n];
+            }
+        }
+
+        return dp[target];
+    }
+}
