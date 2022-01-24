31d30b232cb03ff6a42540eb6f95a3dcc7347ed5
think101 <taohuayuan2000@hotmail.com>
2022-01-23 22:45:58 -0800

    279. Perfect Squares

--- src/main/com/think101/leetcode/PerfectSquares/Solution.java
+++ src/main/com/think101/leetcode/PerfectSquares/Solution.java
@@ -1,8 +1,38 @@
 package main.com.think101.leetcode.PerfectSquares;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Solution {
 
     public int numSquares(int n) {
+        int[] dp = new int[n+1];
+        dp[1] = 1;
+
+        List<Integer> squares = new ArrayList<>();
+        for(int i=1; i<=100; i++)
+            squares.add(i * i);
+
+        for(int i=2; i<=n; i++) {
+            dp[i] = i;
+            if(squares.contains(i)) {
+                dp[i] = 1;
+                continue;
+            }
+
+            for(int s : squares) {
+                if(s > i)
+                    break;
+
+                if(dp[i-s] + 1 < dp[i])
+                    dp[i] = dp[i-s] + 1;
+            }
+        }
+
+        return dp[n];
+    }
+
+    public int numSquares_V1(int n) {
         int[] dp = new int[n+1];
         dp[1] = 1;
 
