532c99a26755414a7760d6e1a28df9a57e2acd88
think101 <taohuayuan2000@hotmail.com>
2022-02-07 22:50:07 -0800

    94. Binary Tree Inorder Traversal

---
+++ src/main/com/think101/leetcode/BinaryTreeInorderTraversal/Solution.java
@@ -0,0 +1,32 @@
+package main.com.think101.leetcode.BinaryTreeInorderTraversal;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Solution {
+
+    public List<Integer> inorderTraversal(TreeNode root) {
+        if(root == null)
+            return new ArrayList<>();
+
+        List<Integer> left = inorderTraversal(root.left);
+        left.add(root.val);
+        left.addAll(inorderTraversal(root.right));
+
+        return left;
+    }
+
+    public class TreeNode {
+      int val;
+      TreeNode left;
+      TreeNode right;
+      TreeNode() {}
+      TreeNode(int val) { this.val = val; }
+      TreeNode(int val, TreeNode left, TreeNode right) {
+          this.val = val;
+          this.left = left;
+          this.right = right;
+      }
+  }
+
+}
