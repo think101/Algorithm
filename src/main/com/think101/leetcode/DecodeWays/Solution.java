f4195911a7cd4c75cc22f5a95f88841e5fb5657e
think101 <taohuayuan2000@hotmail.com>
2022-02-09 19:28:35 -0800

    91. Decode Ways, fix dp solution

--- src/main/com/think101/leetcode/DecodeWays/Solution.java
+++ src/main/com/think101/leetcode/DecodeWays/Solution.java
@@ -2,40 +2,41 @@ package main.com.think101.leetcode.DecodeWays;
 
 public class Solution {
     public static void main(String[] args) {
-        System.out.println(numDecodings("227"));
+        System.out.println(numDecodings("301"));
     }
 
     public static int numDecodings(String s) {
-        if(s.length() == 1) return s.charAt(0) == '0' ? 0 : 1;
+        int[] dp = new int[s.length() +1];
+        dp[0] = 1;
 
-        int[] dp = new int[s.length()];
-        dp[0] = s.charAt(0) == '0' ? 0 : 1;
-
-        for(int i=1; i<s.length(); i++) {
-            if(s.charAt(i-1) == '0' || s.charAt(i-1) > '2') {
-                if(s.charAt(i) == '0') return 0;
-
-                dp[i] = dp[i-1];
+        if(s.length()==0)
+            return 1;
+        else if(s.charAt(0) == '0')
+            return 0;
+        else if(s.length() == 1)
+            dp[1] = 1;
+        else {
+            dp[1] = 1;
+            if(s.charAt(0) > '2' && s.charAt(1) == '0')
+                dp[2] = 0;
+            else if(s.charAt(0)>'2' || (s.charAt(0)=='2' && s.charAt(1)>'6')
+                    || s.charAt(1)=='0') {
+                dp[2] = 1;
             }
-            else {
-                if(i-2>=0) {
-                    if(s.charAt(i) == '0')
-                        dp[i] = dp[i-2];
-                    else if(s.charAt(i) > '6')
-                        dp[i] = dp[i-1];
-                    else
-                        dp[i] = dp[i-2] + dp[i-1];
-                }
-                else {
-                    if(s.charAt(i) == '0' || s.charAt(i) > '6')
-                        dp[i] = dp[i-1];
-                    else
-                        dp[i] = dp[i-1] + 1;
-                }
-            }
+            else
+                dp[2] = 2;
         }
 
-        return dp[s.length()-1];
+        for(int i=3; i<=s.length(); i++) {
+            int t = Integer.parseInt(s.substring(i-2, i));
+            if(t>=10 && t<=26)
+                dp[i] += dp[i-2];
+
+            if(s.charAt(i-1) != '0')
+                dp[i] += dp[i-1];
+        }
+
+        return dp[s.length()];
     }
 
 
