995499731fa605c2eb3b2427b3d7d7297dedaae4
think101 <taohuayuan2000@hotmail.com>
2022-03-25 23:01:44 -0700

    143. Reorder List non recursion solution

--- src/main/com/think101/leetcode/ReorderList/Solution.java
+++ src/main/com/think101/leetcode/ReorderList/Solution.java
@@ -41,4 +41,54 @@ public class Solution {
             this.next = next;
         }
     }
+
+
+    public void reorderList2(ListNode head) {
+        ListNode left = head, right = head;
+        while(right != null && right.next != null) {
+            left = left.next;
+            right = right.next.next;
+        }
+
+        ListNode rightPart = reverse(left.next);
+        left.next = null;
+        merge(head, rightPart);
+    }
+
+    private ListNode reverse(ListNode head) {
+        if(head == null || head.next == null)
+            return head;
+
+        ListNode current = head, next = head.next;
+        head.next = null;
+        while(next != null) {
+            ListNode tmp = next.next;
+            next.next = current;
+            current = next;
+            next = tmp;
+        }
+
+        return current;
+    }
+
+    private ListNode merge(ListNode node1, ListNode node2) {
+        ListNode result = new ListNode(-1, null);
+        ListNode current = result;
+
+        while(node1 != null || node2 != null) {
+            if(node1 != null) {
+                current.next = node1;
+                node1 = node1.next;
+                current = current.next;
+            }
+
+            if(node2 != null){
+                current.next = node2;
+                node2 = node2.next;
+                current = current.next;
+            }
+        }
+
+        return result.next;
+    }
 }
