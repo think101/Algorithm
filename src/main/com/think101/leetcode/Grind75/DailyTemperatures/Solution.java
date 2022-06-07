f99e05ec5902afd3d33c59e11a0c31a78e64aa1f
think101 <taohuayuan2000@hotmail.com>
2022-06-06 22:50:01 -0700

    739. Daily Temperatures

---
+++ src/main/com/think101/leetcode/Grind75/DailyTemperatures/Solution.java
@@ -0,0 +1,49 @@
+package main.com.think101.leetcode.Grind75.DailyTemperatures;
+
+import java.util.Arrays;
+
+public class Solution {
+
+    public int[] dailyTemperatures(int[] temperatures) {
+        int len = temperatures.length;
+        int[] res = new int[len];
+        res[len-1] = 0;
+
+        for(int i = len - 2; i >= 0; i--){
+            if(temperatures[i+1] > temperatures[i]){
+                res[i] = 1;
+            }
+            else {
+                int ind = i+1;
+                while(true) {
+                    int t = res[ind];
+                    if(t == 0){
+                        ind = 0;
+                        break;
+                    }
+
+                    ind = ind + t;
+                    if(temperatures[ind] > temperatures[i]){
+                        break;
+                    }
+                }
+
+                res[i] = ind > i ? ind-i : 0;
+            }
+        }
+
+        return res;
+    }
+
+    public static void main(String[] args) {
+        Solution s = new Solution();
+        int[] temperatures = new int[]{73, 74, 75, 71, 69, 72, 76, 73};
+        System.out.println(Arrays.toString(s.dailyTemperatures(temperatures)));
+
+        temperatures = new int[]{30, 40, 50, 60};
+        System.out.println(Arrays.toString(s.dailyTemperatures(temperatures)));
+
+        temperatures = new int[]{30, 60, 90};
+        System.out.println(Arrays.toString(s.dailyTemperatures(temperatures)));
+    }
+}
