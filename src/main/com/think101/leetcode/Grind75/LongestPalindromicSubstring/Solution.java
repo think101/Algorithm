b2966fff3f5cb6e1ddc099878e7d47fbd7a70841
think101 <taohuayuan2000@hotmail.com>
2022-06-12 00:14:31 -0700

    5. Longest Palindromic Substring fix bug

--- src/main/com/think101/leetcode/Grind75/LongestPalindromicSubstring/Solution.java
+++ src/main/com/think101/leetcode/Grind75/LongestPalindromicSubstring/Solution.java
@@ -21,7 +21,8 @@ public class Solution {
 
     private String checkPalin(String s, int i){
         // check palindrom str with i as the center
-        StringBuilder sb = new StringBuilder(s.charAt(i));
+        StringBuilder sb = new StringBuilder();
+        sb.append(s.charAt(i));
         int j = 1;
         while(i-j >= 0 && i+j < s.length() && s.charAt(i-j) == s.charAt(i+j)){
             sb.append(s.charAt(i+j));
@@ -29,7 +30,6 @@ public class Solution {
             j++;
         }
         String sub1 = sb.toString();
-        System.out.println(sub1);
 
         //check palindrom str with space between i and i+1 as the center
         sb = new StringBuilder();
@@ -40,14 +40,16 @@ public class Solution {
             j++;
         }
         String sub2 = sb.toString();
-        System.out.println(sub2);
 
         return sub1.length() > sub2.length() ? sub1 : sub2;
     }
 
     public static void main(String[] args) {
         Solution s = new Solution();
-        String s1 = "babad";
-        System.out.println(s.longestPalindrome(s1));
+        System.out.println(s.longestPalindrome("babad"));
+        System.out.println(s.longestPalindrome("cbbd"));
+        System.out.println(s.longestPalindrome("cbbc"));
+        System.out.println(s.longestPalindrome("cccccccc"));
+
     }
 }
