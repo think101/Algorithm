4c5f86d9d69451475ef4a1bef3be92ce7b175387
think101 <68447550+think101@users.noreply.github.com>
2022-06-21 23:56:35 -0700

    Create Solution.java

---
+++ src/main/com/think101/leetcode/contest/SumOfNumberWithUnitDigitK/Solution.java
@@ -0,0 +1,28 @@
+class Solution {
+    public int minimumNumbers(int num, int k) {
+        if(num == 0)
+            return 0;
+        
+        if(num < k)
+            return -1;
+        else if(num == k)
+            return 1;
+        
+        int[] dp = new int[11];
+        dp[0] = 0;
+        dp[1] = k;
+        
+        int r = num % 10; 
+        
+        for(int i = 2; i <= 10; i++){
+            dp[i] = dp[i-1] + k;
+            
+            if(dp[i] % 10 == r  && dp[i] <= num) {
+                return i;
+            }
+        }
+        
+        return -1;
+        
+    }
+}
