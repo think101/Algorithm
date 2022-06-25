4af882f4212b7bed2006c986ca55014439bfa7de
think101 <68447550+think101@users.noreply.github.com>
2022-06-24 23:49:54 -0700

    Create Solution.java

---
+++ src/main/com/think101/leetcode/contest/NimGame/Solution.java
@@ -0,0 +1,9 @@
+class Solution {
+    public boolean canWinNim(int n) {
+        return helper(n-1) || helper(n-2) || helper(n-3);
+    }
+    
+    private boolean helper(int n) {
+        return n >= 0 && (n % 4 == 0 || n % 5 == 0 || n % 6 == 0);
+    }
+}
