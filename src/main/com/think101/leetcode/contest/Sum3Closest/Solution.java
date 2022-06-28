8e620697a3a52cab221d11c4febacf564f19abf8
think101 <taohuayuan2000@hotmail.com>
2022-06-28 00:36:08 -0700

    16. 3Sum Closest wrong answer

--- src/main/com/think101/leetcode/contest/Sum3Closest/Solution.java
+++ src/main/com/think101/leetcode/contest/Sum3Closest/Solution.java
@@ -18,26 +18,39 @@ class Solution {
         int result = Integer.MAX_VALUE;
 
         for(int i = 0; i < cnts.size(); i++) {
-            for(int j = i+1; j < cnts.size(); j++) {
+            for(int j = i; j < cnts.size(); j++) {
+                if(cnts.get(cnts.keySet().toArray()[j]) < 1 + (i == j ? 1 : 0))
+                    continue;
+
                 int t = target - (int)cnts.keySet().toArray()[i] - (int)cnts.keySet().toArray()[j];
 
                 Map.Entry<Integer, Integer> floorEntry =  cnts.floorEntry(t);
-                int floor = floorEntry == null ? 0 : floorEntry.getKey();
-                if(cnts.get(floor) >= (1 + (floor == i ? 1 : 0) + (floor == j ? 1 : 0))
-                        && Math.abs(target - i - j - floor) < Math.abs(target - result)) {
-                    result = i + j + floor;
+                int floor = floorEntry == null ? (int)cnts.keySet().toArray()[0] : floorEntry.getKey();
+                if(cnts.get(floor) >= (1 + (floor == (int)cnts.keySet().toArray()[i] ? 1 : 0) +
+                        (floor == (int)cnts.keySet().toArray()[j] ? 1 : 0))
+                        && Math.abs(target - (int)cnts.keySet().toArray()[i] - (int)cnts.keySet().toArray()[j] - floor)
+                        < Math.abs(target - result)) {
+                    result = (int)cnts.keySet().toArray()[i] + (int)cnts.keySet().toArray()[j] + floor;
                 }
 
                 Map.Entry<Integer, Integer> ceilingEntry =  cnts.ceilingEntry(t);
                 int ceiling = ceilingEntry == null ? (int)cnts.keySet().toArray()[cnts.size() - 1]
                         : ceilingEntry.getKey();
-                if(cnts.get(ceiling) >= (1 + (ceiling == i ? 1 : 0) + (ceiling == j ? 1 : 0))
-                        && Math.abs(target - i - j - ceiling) < Math.abs(target - result)) {
-                    result = i + j + ceiling;
+                if(cnts.get(ceiling) >= (1 + (ceiling == (int)cnts.keySet().toArray()[i] ? 1 : 0) +
+                        (ceiling == (int)cnts.keySet().toArray()[j] ? 1 : 0))
+                        && Math.abs(target - (int)cnts.keySet().toArray()[i] - (int)cnts.keySet().toArray()[j] - ceiling)
+                        < Math.abs(target - result)) {
+                    result = (int)cnts.keySet().toArray()[i] + (int)cnts.keySet().toArray()[j] + ceiling;
                 }
             }
         }
 
         return result;
+    }
+
+    public static void main(String[] args) {
+        Solution s = new Solution();
+        System.out.println(s.threeSumClosest(new int[]{1, 1, -1, -1, 3}, -1));
+
     }
 }
