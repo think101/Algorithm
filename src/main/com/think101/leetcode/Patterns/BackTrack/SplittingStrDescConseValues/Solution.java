b3247d0b0910cac3fa99d7fc3f7c86d97be156d3
think101 <taohuayuan2000@hotmail.com>
2022-07-08 21:17:15 -0700

    1849. Splitting a String Into Descending Consecutive Values fix bug

--- src/main/com/think101/leetcode/Patterns/BackTrack/SplittingStrDescConseValues/Solution.java
+++ src/main/com/think101/leetcode/Patterns/BackTrack/SplittingStrDescConseValues/Solution.java
@@ -1,5 +1,6 @@
 package main.com.think101.leetcode.Patterns.BackTrack.SplittingStrDescConseValues;
 
+import java.math.BigInteger;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -8,14 +9,14 @@ class Solution {
         return dfs(s, 0, new ArrayList<>());
     }
 
-    private boolean dfs(String s, int i, List<Integer> substrs) {
+    private boolean dfs(String s, int i, List<BigInteger> substrs) {
         if (i == s.length() && substrs.size() > 1) {
             return true;
         }
 
         for(int j = i; j < s.length(); j++) {
-            int substrInt = Integer.parseInt(s.substring(i, j+1));
-            if(substrs.size() == 0 || substrInt - substrs.get(substrs.size() - 1) == -1) {
+            BigInteger substrInt = new BigInteger(s.substring(i, j+1));
+            if(substrs.size() == 0 || substrs.get(substrs.size() - 1).subtract(substrInt).equals(BigInteger.ONE)) {
                 substrs.add(substrInt);
                 if(dfs(s, j+1, substrs))
                     return true;
@@ -30,5 +31,7 @@ class Solution {
         Solution s = new Solution();
         System.out.println(s.splitString("12345"));
         System.out.println(s.splitString("050043"));
+        System.out.println(s.splitString("3202872336"));
+        System.out.println(s.splitString("64424509442147483647"));
     }
 }
