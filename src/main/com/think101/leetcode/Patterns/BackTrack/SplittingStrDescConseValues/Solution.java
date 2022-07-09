ad614efc667d4a9625a633c04da7c9bde92d7b33
think101 <taohuayuan2000@hotmail.com>
2022-07-08 20:49:32 -0700

    1849. Splitting a String Into Descending Consecutive Values fix bug

--- src/main/com/think101/leetcode/Patterns/BackTrack/SplittingStrDescConseValues/Solution.java
+++ src/main/com/think101/leetcode/Patterns/BackTrack/SplittingStrDescConseValues/Solution.java
@@ -9,15 +9,16 @@ class Solution {
     }
 
     private boolean dfs(String s, int i, List<Integer> substrs) {
-        if (i == s.length()) {
+        if (i == s.length() && substrs.size() > 1) {
             return true;
         }
 
         for(int j = i; j < s.length(); j++) {
             int substrInt = Integer.parseInt(s.substring(i, j+1));
-            if(substrs.size() == 0 || substrInt - substrs.get(substrs.size() - 1) == 1) {
+            if(substrs.size() == 0 || substrInt - substrs.get(substrs.size() - 1) == -1) {
                 substrs.add(substrInt);
-                dfs(s, j+1, substrs);
+                if(dfs(s, j+1, substrs))
+                    return true;
                 substrs.remove(substrs.size() - 1);
             }
         }
@@ -27,6 +28,7 @@ class Solution {
 
     public static void main(String[] args) {
         Solution s = new Solution();
+        System.out.println(s.splitString("12345"));
         System.out.println(s.splitString("050043"));
     }
 }
