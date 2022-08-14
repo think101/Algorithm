b571994cf94113bf60cb82e6f0180c3230a5f77f
think101 <taohuayuan2000@hotmail.com>
2022-08-13 23:08:50 -0700

    310. Minimum Height Trees bfs still have issue

--- src/main/com/think101/leetcode/Patterns/BFS/MinHeightTrees/Solution.java
+++ src/main/com/think101/leetcode/Patterns/BFS/MinHeightTrees/Solution.java
@@ -1,58 +1,57 @@
 package main.com.think101.leetcode.Patterns.BFS.MinHeightTrees;
 
 import java.util.*;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 
 public class Solution {
     public List<Integer> findMinHeightTrees(int n, int[][] edges) {
-        TreeMap<Integer, List<Integer>> heightToNode = new TreeMap<>();
+        if(n <= 2) return IntStream.rangeClosed(0, n-1).boxed().collect(Collectors.toList());
+
         Map<Integer, List<Integer>> graph = new HashMap<>();
+        Map<Integer, Integer> degrees = new HashMap<>();
 
         for(int[] edge : edges) {
             if(!graph.containsKey(edge[0])){
-                graph.put(edge[0], new ArrayList<Integer>());
+                graph.put(edge[0], new ArrayList<>());
             }
             graph.get(edge[0]).add(edge[1]);
 
             if(!graph.containsKey(edge[1])){
-                graph.put(edge[1], new ArrayList<Integer>());
+                graph.put(edge[1], new ArrayList<>());
             }
             graph.get(edge[1]).add(edge[0]);
         }
 
-
-        for(int i = 0; i < n; i++) {
-            Queue<Integer> q = new LinkedList<>();
-            q.add(i);
-
-            Set<Integer> visited = new HashSet<>();
-
-            int depth = 0;
-            while(q.size() > 0) {
-                depth++;
-
-                int cnt = q.size();
-                for(int j = 0; j < cnt; j++) {
-                    int t = q.poll();
-
-                    if(visited.contains(t))
-                        continue;
-
-                    visited.add(t);
-                    if(graph.containsKey(t)) {
-                        for(int k : graph.get(t))
-                            if(!visited.contains(k))
-                                q.add(k);
-                    }
-                }
+        Queue<Integer> queue = new LinkedList<>();
+        for(int i : graph.keySet()) {
+            if(graph.get(i).size() == 1) {
+                queue.add(i);
+                continue;
             }
 
-            if(!heightToNode.containsKey(depth)){
-                heightToNode.put(depth, new ArrayList<Integer>());
-            }
-
-            heightToNode.get(depth).add(i);
+            degrees.put(i, graph.get(i).size());
         }
 
-        return heightToNode.firstEntry().getValue();
+        while(queue.size() > 0) {
+            if(degrees.size() <= 2) {
+                break;
+            }
+
+            int t = queue.poll();
+            for(int neighbor : graph.get(t)) {
+                degrees.put(neighbor, degrees.get(neighbor) - 1);
+                if(degrees.get(neighbor) == 1) {
+                    queue.add(neighbor);
+                    degrees.remove(neighbor);
+                }
+            }
+        }
+
+        return new ArrayList<>(degrees.keySet());
+    }
+
+    public static void main(String[] args) {
+        System.out.println(new Solution().findMinHeightTrees(6, new int[][] {{0,1},{0,2},{0,3}, {3,4}, {4,5}}));
     }
 }
