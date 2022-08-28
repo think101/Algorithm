9298e3bf13394933fa1994c6e92ab43d4fd91aaf
think101 <taohuayuan2000@hotmail.com>
2022-08-28 11:21:20 -0700

    731. My Calendar II half way commit

--- src/main/com/think101/leetcode/Patterns/SegmentTree/MyCalendarII/MyCalendarTwo.java
+++ src/main/com/think101/leetcode/Patterns/SegmentTree/MyCalendarII/MyCalendarTwo.java
@@ -1,5 +1,6 @@
 package main.com.think101.leetcode.Patterns.SegmentTree.MyCalendarII;
 
+import java.util.Map;
 import java.util.TreeMap;
 
 public class MyCalendarTwo {
@@ -12,19 +13,24 @@ public class MyCalendarTwo {
     }
 
     public boolean book(int start, int end) {
-        if(singleBooking.size() == 0 || doubleBooking.size() == 0){
+        if(singleBooking.size() == 0 && doubleBooking.size() == 0){
             singleBooking.put(start,end);
             return true;
         }
 
+        Integer floorKey;
+        Integer ceilingKey;
         // first check whether there is triple booking
-        Integer floorKey = doubleBooking.floorKey(start);
-        Integer ceilingKey = doubleBooking.ceilingKey(start);
+        if(doubleBooking.size() > 0){
+            floorKey = doubleBooking.floorKey(start);
+            ceilingKey = doubleBooking.ceilingKey(start);
 
-        if((floorKey == null && end > doubleBooking.firstKey()) ||
-                (ceilingKey == null && start < doubleBooking.get(doubleBooking.lastKey())) ||
-                start < doubleBooking.get(floorKey) || end > ceilingKey) {
-            return false;
+            if((floorKey == null && end > doubleBooking.firstKey()) ||
+                    (ceilingKey == null && start < doubleBooking.get(doubleBooking.lastKey())) ||
+                    (floorKey != null && start < doubleBooking.get(floorKey)) ||
+                    (ceilingKey != null && end > ceilingKey)) {
+                return false;
+            }
         }
 
         // chech whether new double booking created
@@ -34,16 +40,30 @@ public class MyCalendarTwo {
         // if no overlap with others
         if((floorKey == null && end <= singleBooking.firstKey()) ||
                 (ceilingKey == null && start >= singleBooking.get(singleBooking.lastKey())) ||
-                (start >= singleBooking.get(floorKey) && end <= ceilingKey)) {
+                (floorKey != null && start >= singleBooking.get(floorKey)
+                        && ceilingKey != null && end <= ceilingKey)) {
             singleBooking.put(start,end);
             return true;
         }
         else {
-
+            for(Map.Entry<Integer, Integer> entry : singleBooking.entrySet()) {
+                if( !(start >= entry.getValue() || end <= entry.getKey())) {
+                    doubleBooking.put(Math.max(start, entry.getKey()), Math.min(end, entry.getValue()));
+                }
+            }
         }
 
-        return false;
+        return true;
+    }
 
+    public static void main(String[] args) {
+        MyCalendarTwo myCalendarTwo = new MyCalendarTwo();
+        System.out.println(myCalendarTwo.book(10, 20));
+        System.out.println(myCalendarTwo.book(50, 60));
+        System.out.println(myCalendarTwo.book(10, 40));
+        System.out.println(myCalendarTwo.book(5, 15));
+        System.out.println(myCalendarTwo.book(5, 10));
+        System.out.println(myCalendarTwo.book(25, 55));
     }
 }
 
