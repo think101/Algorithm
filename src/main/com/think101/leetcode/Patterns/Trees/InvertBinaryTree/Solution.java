ae442c84547491c0a5a6d250248f1fe876a2ebc9
think101 <taohuayuan2000@hotmail.com>
2022-09-25 10:01:53 -0700

    226. Invert Binary Tree

---
+++ src/main/com/think101/leetcode/Patterns/Trees/InvertBinaryTree/Solution.java
@@ -0,0 +1,48 @@
+package main.com.think101.leetcode.Patterns.Trees.InvertBinaryTree;
+
+class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+
+    TreeNode(int val) {
+        this.val = val;
+    }
+
+}
+
+public class Solution {
+    public TreeNode invertTree(TreeNode root) {
+        if(root == null || (root.left == null && root.right == null)) return root;
+
+        TreeNode newLeft = invertTree(root.right);
+        TreeNode newRight = invertTree(root.left);
+
+        root.left = newLeft;
+        root.right = newRight;
+
+        return root;
+    }
+
+    public static void main(String[] args) {
+        Solution s = new Solution();
+        TreeNode root = new TreeNode(4);
+        root.left = new TreeNode(2);
+        root.right = new TreeNode(7);
+        root.left.left = new TreeNode(1);
+        root.left.right = new TreeNode(3);
+        root.right.left = new TreeNode(6);
+        root.right.right = new TreeNode(9);
+        printTree(root);
+        System.out.println();
+        s.invertTree(root);
+        printTree(root);
+    }
+
+    private static void printTree(TreeNode root) {
+        if(root == null) return;
+        System.out.print(root.val + " ");
+        printTree(root.left);
+        printTree(root.right);
+    }
+}
