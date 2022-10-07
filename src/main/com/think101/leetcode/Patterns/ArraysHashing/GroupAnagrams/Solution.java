8bd4e0c1e7bef2b1a79c0d58689f78e5a11d894f
think101 <taohuayuan2000@hotmail.com>
2022-10-06 21:43:45 -0700

    49. Group Anagrams

---
+++ src/main/com/think101/leetcode/Patterns/ArraysHashing/GroupAnagrams/Solution.java
@@ -0,0 +1,29 @@
+package main.com.think101.leetcode.Patterns.ArraysHashing.GroupAnagrams;
+
+import java.util.*;
+
+public class Solution {
+    public List<List<String>> groupAnagrams(String[] strs) {
+        Map<String, List<String>> res = new HashMap<>();
+
+        for(String s : strs) {
+            int[] a = new int[26];
+
+            for(int i = 0; i < s.length(); i++)
+                a[s.charAt(i) - 'a']++;
+
+            String as = Arrays.toString(a);
+            if(!res.containsKey(as))
+                res.put(as, new ArrayList<>());
+
+            res.get(as).add(s);
+        }
+
+        return new ArrayList<>(res.values());
+    }
+
+    public static void main(String[] args) {
+        Solution s = new Solution();
+        System.out.println(s.groupAnagrams(new String[]{"eat", "tea", "tan", "ate", "nat", "bat"}));
+    }
+}
