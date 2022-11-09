46c3658df7038c22c06428a490460ffddb0dbc9f
think101 <taohuayuan2000@hotmail.com>
2022-11-09 00:06:10 -0800

    981. Time Based Key-Value Store Solution 2 fix issues

--- src/main/com/think101/leetcode/Patterns/BinarySearch/TimeBasedKeyValueStore/TimeMap2.java
+++ src/main/com/think101/leetcode/Patterns/BinarySearch/TimeBasedKeyValueStore/TimeMap2.java
@@ -1,11 +1,17 @@
 package main.com.think101.leetcode.Patterns.BinarySearch.TimeBasedKeyValueStore;
 
+import org.apache.commons.lang3.tuple.ImmutablePair;
 import org.apache.commons.lang3.tuple.Pair;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 public class TimeMap2 {
     Map<String, List<Pair<Integer, String>>> store;
 
-    public TimeMap() {
+    public TimeMap2() {
         store = new HashMap<>();
     }
 
@@ -14,7 +20,7 @@ public class TimeMap2 {
             store.put(key, new ArrayList<>());
         }
 
-        store.get(key).add(new Pair<>(timestamp, value));
+        store.get(key).add(new ImmutablePair<>(timestamp, value));
     }
 
     public String get(String key, int timestamp) {
@@ -24,14 +30,24 @@ public class TimeMap2 {
             int l = 0, r = m.size() - 1;
             while(l <= r) {
                 int middle = (l + r) / 2;
-                if(m.get(middle).first() == timestamp) return m.get(middle).second();
-                else if(m.get(middle).first() < timestamp) l = middle + 1;
+                if(m.get(middle).getLeft() == timestamp) return m.get(middle).getRight();
+                else if(m.get(middle).getLeft() < timestamp) l = middle + 1;
                 else r = middle - 1;
             }
 
-            if(r >= 0 && m.get(r).first() < timestamp) return m.get(r).second();
+            if(r >= 0 && m.get(r).getLeft() < timestamp) return m.get(r).getRight();
         }
 
         return "";
+    }
+
+    public static void main(String[] args) {
+        TimeMap2 s = new TimeMap2();
+        s.set("foo", "bar", 1);
+        System.out.println(s.get("foo", 1));
+        System.out.println(s.get("foo", 3));
+        s.set("foo", "bar2", 4);
+        System.out.println(s.get("foo", 4));
+        System.out.println(s.get("foo", 5));
     }
 }
