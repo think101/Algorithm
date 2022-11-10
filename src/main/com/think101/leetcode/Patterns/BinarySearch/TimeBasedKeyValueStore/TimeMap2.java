30c39ed6820debfcb983e15b8da99cca18e162e6
think101 <taohuayuan2000@hotmail.com>
2022-11-09 21:10:39 -0800

    981. Time Based Key-Value Store Solution 2 use Node not TreeMap, also note Pair is not included in the class path so can not be used here

--- src/main/com/think101/leetcode/Patterns/BinarySearch/TimeBasedKeyValueStore/TimeMap2.java
+++ src/main/com/think101/leetcode/Patterns/BinarySearch/TimeBasedKeyValueStore/TimeMap2.java
@@ -1,15 +1,31 @@
 package main.com.think101.leetcode.Patterns.BinarySearch.TimeBasedKeyValueStore;
 
-import org.apache.commons.lang3.tuple.ImmutablePair;
-import org.apache.commons.lang3.tuple.Pair;
-
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
 public class TimeMap2 {
-    Map<String, List<Pair<Integer, String>>> store;
+    class Node {
+        private final int timestamp;
+        private final String value;
+
+        public Node(int t, String v) {
+            this.timestamp = t;
+            this.value = v;
+        }
+
+        public int getTimeStamp() {
+            return timestamp;
+        }
+
+        public String getValue() {
+            return value;
+        }
+    }
+
+
+    Map<String, List<Node>> store;
 
     public TimeMap2() {
         store = new HashMap<>();
@@ -20,22 +36,22 @@ public class TimeMap2 {
             store.put(key, new ArrayList<>());
         }
 
-        store.get(key).add(new ImmutablePair<>(timestamp, value));
+        store.get(key).add(new Node(timestamp, value));
     }
 
     public String get(String key, int timestamp) {
-        List<Pair<Integer, String>> m = store.get(key);
+        List<Node> m = store.get(key);
 
         if(m != null) {
             int l = 0, r = m.size() - 1;
             while(l <= r) {
                 int middle = (l + r) / 2;
-                if(m.get(middle).getLeft() == timestamp) return m.get(middle).getRight();
-                else if(m.get(middle).getLeft() < timestamp) l = middle + 1;
+                if(m.get(middle).getTimeStamp() == timestamp) return m.get(middle).getValue();
+                else if(m.get(middle).getTimeStamp() < timestamp) l = middle + 1;
                 else r = middle - 1;
             }
 
-            if(r >= 0 && m.get(r).getLeft() < timestamp) return m.get(r).getRight();
+            if(r >= 0 && m.get(r).getTimeStamp() < timestamp) return m.get(r).getValue();
         }
 
         return "";
