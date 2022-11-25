c3b231bcb435a7d461afb6eb84c88f1c82d94d68
think101 <taohuayuan2000@hotmail.com>
2022-11-24 21:23:40 -0800

    79. Word Search avoid use visited set but change board content to resolve TLE issue

--- src/main/com/think101/leetcode/Patterns/BackTrack/WordSearch/Solution2.java
+++ src/main/com/think101/leetcode/Patterns/BackTrack/WordSearch/Solution2.java
@@ -1,12 +1,10 @@
 package main.com.think101.leetcode.Patterns.BackTrack.WordSearch;
 
-import java.util.*;
-
 public class Solution2 {
     public boolean exist(char[][] board, String word) {
         for(int i = 0; i < board.length; i ++) {
             for(int j = 0; j < board[0].length; j++) {
-                if(dfs(board, word, 0, i, j, new HashSet<>())){
+                if(dfs(board, word, 0, i, j)){
                     return true;
                 }
             }
@@ -15,17 +13,18 @@ public class Solution2 {
         return false;
     }
 
-    private boolean dfs(char[][] board, String word, int ind, int i, int j, Set<List<Integer>> visited) {
+    private boolean dfs(char[][] board, String word, int ind, int i, int j) {
         if(ind == word.length()) return true;
         if(i < 0 || j < 0 || i >= board.length || j >= board[0].length) return false;
         if(board[i][j] != word.charAt(ind)) return false;
 
-        visited.add(Arrays.asList(i, j));
-        if(!visited.contains(Arrays.asList(i - 1, j)) && dfs(board, word, ind+1, i - 1, j, visited)) return true;
-        if(!visited.contains(Arrays.asList(i + 1, j)) && dfs(board, word, ind+1, i + 1, j, visited)) return true;
-        if(!visited.contains(Arrays.asList(i, j - 1)) && dfs(board, word, ind+1, i, j - 1, visited)) return true;
-        if(!visited.contains(Arrays.asList(i, j + 1)) && dfs(board, word, ind+1, i, j + 1, visited)) return true;
-        visited.remove(Arrays.asList(i, j));
+        char c = board[i][j];
+        board[i][j] = '0';
+        if(dfs(board, word, ind+1, i - 1, j)) return true;
+        if(dfs(board, word, ind+1, i + 1, j)) return true;
+        if(dfs(board, word, ind+1, i, j - 1)) return true;
+        if(dfs(board, word, ind+1, i, j + 1)) return true;
+        board[i][j] = c;
 
         return false;
     }
