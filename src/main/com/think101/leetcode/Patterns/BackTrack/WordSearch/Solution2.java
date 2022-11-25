6b9a0681d6bb047d20a6942715954ff8d207cfe5
think101 <taohuayuan2000@hotmail.com>
2022-11-24 21:01:22 -0800

    79. Word Search change to Set still TLE solution

--- src/main/com/think101/leetcode/Patterns/BackTrack/WordSearch/Solution2.java
+++ src/main/com/think101/leetcode/Patterns/BackTrack/WordSearch/Solution2.java
@@ -1,14 +1,12 @@
 package main.com.think101.leetcode.Patterns.BackTrack.WordSearch;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
+import java.util.*;
 
 public class Solution2 {
     public boolean exist(char[][] board, String word) {
         for(int i = 0; i < board.length; i ++) {
             for(int j = 0; j < board[0].length; j++) {
-                if(dfs(board, word, 0, i, j, new ArrayList<>())){
+                if(dfs(board, word, 0, i, j, new HashSet<>())){
                     return true;
                 }
             }
@@ -17,7 +15,7 @@ public class Solution2 {
         return false;
     }
 
-    private boolean dfs(char[][] board, String word, int ind, int i, int j, List<List<Integer>> visited) {
+    private boolean dfs(char[][] board, String word, int ind, int i, int j, Set<List<Integer>> visited) {
         if(ind == word.length()) return true;
         if(i < 0 || j < 0 || i >= board.length || j >= board[0].length) return false;
         if(board[i][j] != word.charAt(ind)) return false;
@@ -27,7 +25,7 @@ public class Solution2 {
         if(!visited.contains(Arrays.asList(i + 1, j)) && dfs(board, word, ind+1, i + 1, j, visited)) return true;
         if(!visited.contains(Arrays.asList(i, j - 1)) && dfs(board, word, ind+1, i, j - 1, visited)) return true;
         if(!visited.contains(Arrays.asList(i, j + 1)) && dfs(board, word, ind+1, i, j + 1, visited)) return true;
-        visited.remove(visited.size() - 1);
+        visited.remove(Arrays.asList(i, j));
 
         return false;
     }
