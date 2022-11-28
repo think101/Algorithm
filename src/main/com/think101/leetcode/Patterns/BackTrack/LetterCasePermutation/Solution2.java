4cc614dd31a5d124f1b72b088f790dc9fa7b62ba
think101 <taohuayuan2000@hotmail.com>
2022-11-27 21:29:36 -0800

    17. Letter Combinations of a Phone Number

---
+++ src/main/com/think101/leetcode/Patterns/BackTrack/LetterCasePermutation/Solution2.java
@@ -0,0 +1,44 @@
+package main.com.think101.leetcode.Patterns.BackTrack.LetterCasePermutation;
+
+import java.util.*;
+
+public class Solution2 {
+    private static final Map<Character, List<String>> numToChars = new HashMap<>();
+    static {
+        numToChars.put('2', Arrays.asList("a", "b", "c"));
+        numToChars.put('3', Arrays.asList("d", "e", "f"));
+        numToChars.put('4', Arrays.asList("g", "h", "i"));
+        numToChars.put('5', Arrays.asList("j", "k", "l"));
+        numToChars.put('6', Arrays.asList("m", "n", "o"));
+        numToChars.put('7', Arrays.asList("p", "q", "r", "s"));
+        numToChars.put('8', Arrays.asList("t", "u", "v"));
+        numToChars.put('9', Arrays.asList("w", "x", "y", "z"));
+    }
+
+    List<String> res = new ArrayList<>();
+
+    public List<String> letterCombinations(String digits) {
+        bt(digits, 0, "");
+        return res;
+    }
+
+    private void bt(String digits, int i, String curr) {
+        if(i >= digits.length()) {
+            if(curr.length() > 0)
+                res.add(curr);
+            return;
+        }
+
+        List<String> chars = numToChars.get(digits.charAt(i));
+        for(String s : chars) {
+            curr += s;
+            bt(digits, i + 1, curr);
+            curr = curr.substring(0, curr.length() - 1);
+        }
+    }
+
+    public static void main(String[] args) {
+        Solution2 s = new Solution2();
+        System.out.println(s.letterCombinations("23"));
+    }
+}
