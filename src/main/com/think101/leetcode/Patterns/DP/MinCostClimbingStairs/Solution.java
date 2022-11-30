3526a31ea83db55bf488bd819b3fa6f3922aa360
think101 <taohuayuan2000@hotmail.com>
2022-11-29 22:54:44 -0800

    746. Min Cost Climbing Stairs

---
+++ src/main/com/think101/leetcode/Patterns/DP/MinCostClimbingStairs/Solution.java
@@ -0,0 +1,20 @@
+package main.com.think101.leetcode.Patterns.DP.MinCostClimbingStairs;
+
+public class Solution {
+    public int minCostClimbingStairs(int[] cost) {
+        int[] dp = new int[cost.length];
+        dp[0] = cost[0];
+        dp[1] = cost[1];
+
+        for(int i = 2; i < cost.length; i++) {
+            dp[i] = cost[i] + Math.min(dp[i-1], dp[i-2]);
+        }
+
+        return Math.min(dp[cost.length - 1], dp[cost.length - 2]);
+    }
+
+    public static void main(String[] args) {
+        Solution s = new Solution();
+        System.out.println(s.minCostClimbingStairs(new int[]{10, 15, 20}));
+    }
+}
