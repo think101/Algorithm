a6aa6ea9cfe2f73e0d8ec22c9d0e4d9ed019c59b
think101 <taohuayuan2000@hotmail.com>
2022-12-06 20:58:53 -0800

    5. Longest Palindromic Substring DP solution add comments

--- src/main/com/think101/leetcode/Patterns/DP/LongestPalindromicSubstring/SolutionDP.java
+++ src/main/com/think101/leetcode/Patterns/DP/LongestPalindromicSubstring/SolutionDP.java
@@ -6,9 +6,13 @@ public class SolutionDP {
         if(s == null || s.length() == 0) return "";
 
         int n = s.length();
+
+        // dp[i][j] = true if s[i...j] is a palindrome
         boolean[][] dp = new boolean[n][n];
         String res = "";
 
+        // first calculate diagonal, then calculate upper diagonal and so on
+        // if j - i < 3, then s[i...j] is a palindrome if s[i] == s[j]
         for(int i = n - 1; i >= 0; i--) {
             for(int j = i; j < n; j++) {
                 dp[i][j] = s.charAt(i) == s.charAt(j) && (j - i < 3 || dp[i+1][j-1]);
