6a28a439882c3b128034d279347cbe29bd7f5553
think101 <taohuayuan2000@hotmail.com>
2022-12-12 14:45:21 -0800

    152. Maximum Product Subarray correct solution

--- src/main/com/think101/leetcode/Patterns/DP/MaximumProductSubarray/Solution.java
+++ src/main/com/think101/leetcode/Patterns/DP/MaximumProductSubarray/Solution.java
@@ -2,12 +2,14 @@ package main.com.think101.leetcode.Patterns.DP.MaximumProductSubarray;
 
 public class Solution {
     public int maxProduct(int[] nums) {
-        int[] dp = new int[nums.length];
+        int[] max = new int[nums.length];
+        int[] min = new int[nums.length];
         int res = Integer.MIN_VALUE;
 
         for(int i = 0; i < nums.length; i++) {
-            dp[i] = i - 1 >= 0 ? Math.max(nums[i], dp[i-1] * nums[i]) : nums[i];
-            res = Math.max(res, dp[i]);
+            max[i] = i - 1 >= 0 ? Math.max(nums[i], Math.max(max[i-1] * nums[i], min[i-1] * nums[i])) : nums[i];
+            min[i] = i - 1 >= 0 ? Math.min(nums[i], Math.min(max[i-1] * nums[i], min[i-1] * nums[i])) : nums[i];
+            res = Math.max(res, max[i]);
         }
 
         return res;
