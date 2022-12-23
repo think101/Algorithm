024ae4d4dba88b88b836355ead28e16398dcaa71
think101 <taohuayuan2000@hotmail.com>
2022-12-22 23:09:19 -0800

    295. Find Median from Data Stream fix issue

--- src/main/com/think101/leetcode/Patterns/HeapPriorityQueue/FindMedianFromDataStream/MedianFinder.java
+++ src/main/com/think101/leetcode/Patterns/HeapPriorityQueue/FindMedianFromDataStream/MedianFinder.java
@@ -4,8 +4,8 @@ import java.util.Collections;
 import java.util.PriorityQueue;
 
 public class MedianFinder {
-    private PriorityQueue<Integer> minHeap;
-    private PriorityQueue<Integer> maxHeap;
+    private final PriorityQueue<Integer> minHeap;
+    private final PriorityQueue<Integer> maxHeap;
 
     public MedianFinder() {
         this.minHeap = new PriorityQueue<>();
@@ -13,20 +13,24 @@ public class MedianFinder {
     }
 
     public void addNum(int num) {
-        minHeap.add(num);
+        maxHeap.add(num);
 
-        if(minHeap.size() - maxHeap.size() > 1) {
-            int t = minHeap.poll();
-            maxHeap.add(t);
+        if(maxHeap.size() - minHeap.size() > 1) {
+            int t = maxHeap.poll();
+            minHeap.add(t);
+        }
+
+        if(minHeap.size() > 0 && maxHeap.peek() > minHeap.peek()) {
+            int t1 = maxHeap.poll();
+            int t2 = minHeap.poll();
+            maxHeap.add(t2);
+            minHeap.add(t1);
         }
     }
 
     public double findMedian() {
         if(minHeap.size() == maxHeap.size()) {
             return (minHeap.peek() + maxHeap.peek()) / 2.0;
-        }
-        else if(minHeap.size() > maxHeap.size()) {
-            return minHeap.peek();
         }
         else {
             return maxHeap.peek();
