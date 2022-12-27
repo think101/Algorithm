59ef337b0f5bc07690fe58ea45745007533e1223
think101 <taohuayuan2000@hotmail.com>
2022-12-26 22:37:16 -0800

    1405. Longest Happy String init commit

---
+++ src/main/com/think101/leetcode/Patterns/HeapPriorityQueue/LongestHappyString/Solution.java
@@ -0,0 +1,50 @@
+package main.com.think101.leetcode.Patterns.HeapPriorityQueue.LongestHappyString;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.PriorityQueue;
+
+public class Solution {
+    public String longestDiverseString(int a, int b, int c) {
+        PriorityQueue<int[]> pq = new PriorityQueue<>((m, n) -> n[1] - m[1]);
+        List<int[]> q = new ArrayList<>();
+        StringBuilder sb = new StringBuilder();
+        int t = 0;
+
+        pq.add(new int[]{0, a, 0});
+        pq.add(new int[]{1, b, 0});
+        pq.add(new int[]{2, c, 0});
+
+        while(pq.size() > 0 || q.size() > 0) {
+            if(q.size() > 0 && q.get(0)[2] <= t) {
+                int[] elem = q.get(0);
+                q.remove(0);
+                pq.add(elem);
+            }
+
+            if(pq.size() > 0) {
+                int[] elem = pq.poll();
+                char ch = (char)(elem[0] + 'a');
+                if(sb.length() > 1 && sb.charAt(sb.length() - 1) == ch && sb.charAt(sb.length() - 2) == ch) {
+                    if(elem[1] - 1 > 0) {
+                        q.add(new int[]{elem[0], elem[1] - 1, t + 2});
+                    }
+                }
+                else {
+                    sb.append(ch);
+                    q.add(new int[]{elem[0], elem[1] - 1,  t + 1});
+                }
+
+                t++;
+            }
+        }
+
+        return sb.toString();
+
+    }
+
+    public static void main(String[] args) {
+        Solution s = new Solution();
+        System.out.println(s.longestDiverseString(1, 1, 7));
+    }
+}
