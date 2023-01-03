0a7662b6e026280322a76d40e733bd644034ef93
think101 <taohuayuan2000@hotmail.com>
2023-01-02 22:16:38 -0800

    424. Longest Repeating Character Replacement init commit

---
+++ src/main/com/think101/leetcode/Patterns/SlidingWindow/LongestRepeatingCharacterReplacement/Solution.java
@@ -0,0 +1,53 @@
+package main.com.think101.leetcode.Patterns.SlidingWindow.LongestRepeatingCharacterReplacement;
+
+import java.util.Collections;
+import java.util.TreeMap;
+
+public class Solution {
+    public int characterReplacement(String s, int k) {
+        TreeMap<Character, Integer> charCnt = new TreeMap<>(Collections.reverseOrder());
+        int l = 0, r = 0;
+        int res = 0;
+
+        charCnt.put(s.charAt(0), 1);
+
+        while(l < s.length() && r < s.length()) {
+            if(r - l + 1 < res) break;
+
+            int maxCnt = charCnt.firstEntry().getValue();
+            int curr = r - l + 1;
+
+            if(curr - maxCnt <= k) {
+                res = Math.max(res, curr);
+                r++;
+
+                if(r < s.length()) {
+                    if(!charCnt.containsKey(s.charAt(r))) {
+                        charCnt.put(s.charAt(r), 0);
+                    }
+
+                    charCnt.put(s.charAt(r), charCnt.get(s.charAt(r)) + 1);
+                }
+            }
+            else {
+                l++;
+
+                if(charCnt.get(s.charAt(l)).equals(1)) {
+                    charCnt.remove(s.charAt(l));
+                }
+                else {
+                    charCnt.put(s.charAt(l), charCnt.get(s.charAt(l)) - 1);
+                }
+            }
+        }
+
+        return res;
+
+    }
+
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+        System.out.println(solution.characterReplacement("ABAB", 2));
+        System.out.println(solution.characterReplacement("AABABBA", 1));
+    }
+}
