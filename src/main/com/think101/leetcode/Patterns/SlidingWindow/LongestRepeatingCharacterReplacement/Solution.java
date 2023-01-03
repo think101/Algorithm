d7603acd5f321c8921396cc34752da5828f59e93
think101 <taohuayuan2000@hotmail.com>
2023-01-02 22:58:04 -0800

    424. Longest Repeating Character Replacement partial fix

--- src/main/com/think101/leetcode/Patterns/SlidingWindow/LongestRepeatingCharacterReplacement/Solution.java
+++ src/main/com/think101/leetcode/Patterns/SlidingWindow/LongestRepeatingCharacterReplacement/Solution.java
@@ -1,11 +1,11 @@
 package main.com.think101.leetcode.Patterns.SlidingWindow.LongestRepeatingCharacterReplacement;
 
-import java.util.Collections;
+import java.util.Map;
 import java.util.TreeMap;
 
 public class Solution {
     public int characterReplacement(String s, int k) {
-        TreeMap<Character, Integer> charCnt = new TreeMap<>(Collections.reverseOrder());
+        TreeMap<Character, Integer> charCnt = new TreeMap<>();
         int l = 0, r = 0;
         int res = 0;
 
@@ -14,7 +14,7 @@ public class Solution {
         while(l < s.length() && r < s.length()) {
             if(r - l + 1 < res) break;
 
-            int maxCnt = charCnt.firstEntry().getValue();
+            int maxCnt = getMaxValueEntry(charCnt).getValue();
             int curr = r - l + 1;
 
             if(curr - maxCnt <= k) {
@@ -32,11 +32,11 @@ public class Solution {
             else {
                 l++;
 
-                if(charCnt.get(s.charAt(l)).equals(1)) {
-                    charCnt.remove(s.charAt(l));
+                if(charCnt.get(s.charAt(l-1)).equals(1)) {
+                    charCnt.remove(s.charAt(l-1));
                 }
                 else {
-                    charCnt.put(s.charAt(l), charCnt.get(s.charAt(l)) - 1);
+                    charCnt.put(s.charAt(l-1), charCnt.get(s.charAt(l-1)) - 1);
                 }
             }
         }
@@ -45,9 +45,20 @@ public class Solution {
 
     }
 
+    private Map.Entry<Character, Integer> getMaxValueEntry(TreeMap<Character, Integer> charCnt) {
+        Map.Entry<Character, Integer> maxEntry = null;
+        for(Map.Entry<Character, Integer> entry : charCnt.entrySet()) {
+            if(maxEntry == null || entry.getValue() > maxEntry.getValue()) {
+                maxEntry = entry;
+            }
+        }
+
+        return maxEntry;
+    }
+
     public static void main(String[] args) {
         Solution solution = new Solution();
-        System.out.println(solution.characterReplacement("ABAB", 2));
-        System.out.println(solution.characterReplacement("AABABBA", 1));
+        System.out.println(solution.characterReplacement("ABAA", 0));
+        //System.out.println(solution.characterReplacement("AABABBA", 1));
     }
 }
