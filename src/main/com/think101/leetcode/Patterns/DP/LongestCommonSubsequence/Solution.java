6a9fcf3f67b339034bbfd615aa64d2fa8522de7b
think101 <taohuayuan2000@hotmail.com>
2023-01-22 00:06:25 -0800

    1143. Longest Common Subsequence fix bug

--- src/main/com/think101/leetcode/Patterns/DP/LongestCommonSubsequence/Solution.java
+++ src/main/com/think101/leetcode/Patterns/DP/LongestCommonSubsequence/Solution.java
@@ -2,7 +2,7 @@ package main.com.think101.leetcode.Patterns.DP.LongestCommonSubsequence;
 
 public class Solution {
     public int longestCommonSubsequence(String text1, String text2) {
-        int m = text1.length(), n = text2.length();
+        int m = text2.length(), n = text1.length();
         int[][] dp = new int[m][n];
 
         for(int j = 0; j < n; j++) {
@@ -15,12 +15,17 @@ public class Solution {
 
         for(int i = 1; i < m; i++) {
             for(int j = 1; j < n; j++) {
-                int t = text1.charAt(j) == text2.charAt(i) ? dp[i-1][j-1] : 0;
+                int t = text1.charAt(j) == text2.charAt(i) ? dp[i-1][j-1] + 1 : 0;
 
                 dp[i][j] = Math.max(t, Math.max(dp[i-1][j], dp[i][j-1]));
             }
         }
 
         return dp[m-1][n-1];
+    }
+
+    public static void main(String[] args) {
+        Solution s = new Solution();
+        System.out.println(s.longestCommonSubsequence("abcde", "ace"));
     }
 }
