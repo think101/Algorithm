e554efdea00809015d0bcd69436eec1a288568c2
think101 <taohuayuan2000@hotmail.com>
2023-01-25 22:45:52 -0800

    309. Best Time to Buy and Sell Stock with Cooldown fix issue

--- src/main/com/think101/leetcode/Patterns/DP/BestTimeBuyAndSellStockWithCooldown/Solution.java
+++ src/main/com/think101/leetcode/Patterns/DP/BestTimeBuyAndSellStockWithCooldown/Solution.java
@@ -1,10 +1,12 @@
 package main.com.think101.leetcode.Patterns.DP.BestTimeBuyAndSellStockWithCooldown;
 
+import java.util.Arrays;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 public class Solution {
-    Map<int[], Integer> dp = new HashMap<>();
+    Map<List<Integer>, Integer> dp = new HashMap<>();
 
     public int maxProfit(int[] prices) {
         return dfs(0, prices, true);
@@ -12,19 +14,20 @@ public class Solution {
 
     private int dfs(int i, int[] prices, boolean buying) {
         if(i >= prices.length) return 0;
-        if(dp.containsKey(new int[]{i, buying ?  1 : 0})) return dp.get(new int[]{i, buying ?  1 : 0});
+        List<Integer> l = Arrays.asList(i, buying ? 1 : 0);
+        if(dp.containsKey(l)) return dp.get(l);
 
         int cooldown = dfs(i+1, prices, buying);
         if(buying) {
             int buy = dfs(i + 1, prices, false) - prices[i];
-            dp.put(new int[]{i, 0}, Math.max(buy, cooldown));
+            dp.put(l, Math.max(buy, cooldown));
         }
         else {
             int sell = dfs(i + 2, prices, true) + prices[i];
-            dp.put(new int[]{i, 1}, Math.max(sell, cooldown));
+            dp.put(l, Math.max(sell, cooldown));
         }
 
-        return dp.get(new int[]{i, buying ? 1 : 0});
+        return dp.get(l);
     }
 
     public static void main(String[] args) {
