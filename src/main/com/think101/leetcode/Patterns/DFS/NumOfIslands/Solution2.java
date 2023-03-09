c7b40817db860a109b69efa60a9e5ffea3dce874
think101 <taohuayuan2000@hotmail.com>
2023-03-08 23:43:06 -0800

    200. Number of Islands

---
+++ src/main/com/think101/leetcode/Patterns/DFS/NumOfIslands/Solution2.java
@@ -0,0 +1,36 @@
+package main.com.think101.leetcode.Patterns.DFS.NumOfIslands;
+
+public class Solution2 {
+    public int numIslands(char[][] grid) {
+        int res = 0;
+
+        for(int i = 0; i < grid.length; i++) {
+            for(int j = 0; j < grid[0].length; j++) {
+                if(grid[i][j] == '1') {
+                    res++;
+                    dfs(grid, i, j);
+                }
+            }
+        }
+
+        return res;
+    }
+
+    private void dfs(char[][] grid, int i, int j) {
+        if(i < 0 || i >= grid.length || j < 0 || j >= grid[0].length || grid[i][j] == '0') return;
+
+        grid[i][j] = '0';
+        dfs(grid, i - 1, j);
+        dfs(grid, i + 1, j);
+        dfs(grid, i, j - 1);
+        dfs(grid, i, j + 1);
+    }
+
+    public static void main(String[] args) {
+        Solution2 s = new Solution2();
+        System.out.println(s.numIslands(
+                new char[][]{{'1','1','1','1','0'},{'1','1','0','1','0'},{'1','1','0','0','0'},{'0','0','0','0','0'}}));
+        System.out.println(s.numIslands(
+                new char[][]{{'1','1','0','0','0'},{'1','1','0','0','0'},{'0','0','1','0','0'},{'0','0','0','1','1'}}));
+    }
+}
