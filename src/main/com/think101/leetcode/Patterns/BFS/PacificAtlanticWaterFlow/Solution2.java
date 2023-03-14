9d40f460752ca7408fd88aea5b1218edef23be8b
think101 <taohuayuan2000@hotmail.com>
2023-03-13 23:36:11 -0700

    417. Pacific Atlantic Water Flow fix TLE issue

--- src/main/com/think101/leetcode/Patterns/BFS/PacificAtlanticWaterFlow/Solution2.java
+++ src/main/com/think101/leetcode/Patterns/BFS/PacificAtlanticWaterFlow/Solution2.java
@@ -24,7 +24,7 @@ public class Solution2 {
 
         for(int i = 0; i < row; i++) {
             for(int j = 0; j < col; j++) {
-                if(pac[i][j] == 1 && atl[i][j] == 1) {
+                if(pac[i][j] == 2 && atl[i][j] == 2) {
                     res.add(Arrays.asList(i, j));
                 }
             }
@@ -41,6 +41,7 @@ public class Solution2 {
             for(int j = 0; j < col; j++) {
                 if(ocean[i][j] == 1) {
                     q.add(new int[]{i, j});
+                    ocean[i][j] = 2;
                 }
             }
         }
@@ -51,11 +52,24 @@ public class Solution2 {
                 int[] pos = q.poll();
                 int r = pos[0], c = pos[1];
 
-                if(r - 1 >= 0 && ocean[r - 1][c] == 0 && heights[r - 1][c] >= heights[r][c]) q.add(new int[]{r - 1, c});
-                if(r + 1 < row && ocean[r + 1][c] == 0 && heights[r + 1][c] >= heights[r][c]) q.add(new int[]{r + 1, c});
-                if(c - 1 >= 0 && ocean[r][c - 1] == 0 && heights[r][c - 1] >= heights[r][c]) q.add(new int[]{r, c - 1});
-                if(c + 1 < col && ocean[r][c + 1] == 0 && heights[r][c + 1] >= heights[r][c]) q.add(new int[]{r, c + 1});
+                if(r - 1 >= 0 && ocean[r - 1][c] == 0 && heights[r - 1][c] >= heights[r][c]) {q.add(new int[]{r - 1, c}); ocean[r - 1][c] = 2;}
+                if(r + 1 < row && ocean[r + 1][c] == 0 && heights[r + 1][c] >= heights[r][c]){q.add(new int[]{r + 1, c}); ocean[r + 1][c] = 2;}
+                if(c - 1 >= 0 && ocean[r][c - 1] == 0 && heights[r][c - 1] >= heights[r][c]) {q.add(new int[]{r, c - 1}); ocean[r][c - 1] = 2;}
+                if(c + 1 < col && ocean[r][c + 1] == 0 && heights[r][c + 1] >= heights[r][c]){q.add(new int[]{r, c + 1}); ocean[r][c + 1] = 2;}
             }
         }
+    }
+
+    public static void main(String[] args) {
+        int[][] heights = new int[][]{
+            {1,2,2,3,5},
+            {3,2,3,4,4},
+            {2,4,5,3,1},
+            {6,7,1,4,5},
+            {5,1,1,2,4}
+        };
+        Solution2 s = new Solution2();
+        List<List<Integer>> res = s.pacificAtlantic(heights);
+        System.out.println(res);
     }
 }
