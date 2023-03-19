c72b35fb4c79910739765c77fc5ca2e8aef62928
think101 <taohuayuan2000@hotmail.com>
2023-03-19 13:47:46 -0700

    207. Course Schedule

---
+++ src/main/com/think101/leetcode/Patterns/BFS/CourseSchedule/Solution3.java
@@ -0,0 +1,50 @@
+package main.com.think101.leetcode.Patterns.BFS.CourseSchedule;
+
+import java.util.*;
+
+public class Solution3 {
+    public boolean canFinish(int numCourses, int[][] prerequisites) {
+        Map<Integer, Set<Integer>> prereqs = new HashMap<>();
+        Map<Integer, Set<Integer>> revPS = new HashMap<>();
+        Queue<Integer> queue = new LinkedList<>();
+
+        for(int[] p : prerequisites) {
+            if(!prereqs.containsKey(p[0])) {
+                prereqs.put(p[0], new HashSet<>());
+            }
+            if(!revPS.containsKey(p[1])) {
+                revPS.put(p[1], new HashSet<>());
+            }
+
+            prereqs.get(p[0]).add(p[1]);
+            revPS.get(p[1]).add(p[0]);
+        }
+
+        for(int i = 0; i < numCourses; i++) {
+            if(!prereqs.containsKey(i))
+                queue.add(i);
+        }
+
+        while(queue.size() > 0) {
+            int c = queue.poll();
+            if(revPS.containsKey(c)) {
+                for(int cDep : revPS.get(c)) {
+                    prereqs.get(cDep).remove(c);
+                    if(prereqs.get(cDep).size() == 0) {
+                        queue.add(cDep);
+                        prereqs.remove(cDep);
+                    }
+                }
+            }
+        }
+
+        return prereqs.size() == 0;
+    }
+
+    public static void main(String[] args) {
+        Solution3 s = new Solution3();
+        System.out.println(s.canFinish(2, new int[][]{{1,0}}));
+        System.out.println(s.canFinish(2, new int[][]{{1,0},{0,1}}));
+        System.out.println(s.canFinish(2, new int[][]{{1,0},{0,1},{0,1}}));
+    }
+}
