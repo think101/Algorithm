dd77b8c364bc1d13cfc4e92689afe710b0571052
think101 <taohuayuan2000@hotmail.com>
2023-04-15 10:06:14 -0700

    179. Largest Number fix wrong solution

--- src/main/com/think101/leetcode/Patterns/ArraysHashing/LargestNumber/Solution.java
+++ src/main/com/think101/leetcode/Patterns/ArraysHashing/LargestNumber/Solution.java
@@ -1,21 +1,22 @@
 package main.com.think101.leetcode.Patterns.ArraysHashing.LargestNumber;
 
-import java.util.TreeSet;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
 
 public class Solution {
     public String largestNumber(int[] nums) {
-        TreeSet<String> s = new TreeSet<>();
 
-        for(int i : nums) {
-            s.add(String.valueOf(i));
-        }
+        List<Integer> sorted = Arrays.stream(nums).boxed().sorted((a, b) -> {
+            String s1 = a + b.toString();
+            String s2 = b + a.toString();
+            return s2.compareTo(s1);
+        }).collect(Collectors.toList());
 
-        StringBuilder sb = new StringBuilder();
-        while(s.size() > 0) {
-            sb.append(s.pollLast());
-        }
+        String res = sorted.stream().map(Object::toString).reduce("", (a, b) -> a + b);
+        res = res.replaceFirst("^0+", "");
 
-        return sb.toString();
+        return res.isEmpty() ? "0" : res;
     }
 
     public static void main(String[] args) {
