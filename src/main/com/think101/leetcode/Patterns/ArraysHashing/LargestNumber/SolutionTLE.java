eb86c9355359b4e0ef9b41e69b121734b06c610d
think101 <taohuayuan2000@hotmail.com>
2023-04-15 09:45:10 -0700

    179. Largest Number init TLE solution add comment

--- src/main/com/think101/leetcode/Patterns/ArraysHashing/LargestNumber/SolutionTLE.java
+++ src/main/com/think101/leetcode/Patterns/ArraysHashing/LargestNumber/SolutionTLE.java
@@ -61,3 +61,21 @@ public class SolutionTLE {
         System.out.println(s.largestNumber(nums));
     }
 }
+
+/*
+ * Time complexity: O(n! * n * m) where n is the length of nums and m is the length of the largest number in nums
+ *
+ * The time complexity of the backtracking solution for generating all permutations of the given array is O(n!),
+ * where n is the length of the input array.
+ *
+ * This is because there are n choices for the first element in the permutation, n-1 choices for the second element,
+ * n-2 choices for the third element, and so on, until only one choice remains for the last element.
+ * Therefore, the total number of permutations is n * (n-1) * (n-2) * ... * 1, which is n!.
+ *
+ * For each permutation, we need to concatenate the elements into a string using String.join and then compare it with
+ * the current largest string using compareTo. The time complexity of concatenating the elements into a string is O(n),
+ * where n is the length of the permutation. The compareTo method has a time complexity of O(m),
+ * where m is the length of the strings being compared.
+ *
+ * Since we are generating all permutations and comparing them, the overall time complexity of the solution is O(n! * n * m).
+ */
