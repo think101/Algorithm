4e32bdbd0c826733a4b0ed9ce43dbfc5e5f9e30d
think101 <taohuayuan2000@hotmail.com>
2023-04-18 20:37:57 -0700

    560. Subarray Sum Equals K wrong solution

---
+++ src/main/com/think101/leetcode/Patterns/ArraysHashing/SubarraySumEqualsK/Solution.java
@@ -0,0 +1,30 @@
+package main.com.think101.leetcode.Patterns.ArraysHashing.SubarraySumEqualsK;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class Solution {
+    public int subarraySum(int[] nums, int k) {
+        Map<Integer, List<Integer>> sumIndex = new HashMap<>();
+        int sum = 0, res = 0;
+
+        for(int i = 0; i < nums.length; i++) {
+            sum += nums[i];
+
+            if(sumIndex.containsKey(sum - k)) res += sumIndex.size();
+            if(!sumIndex.containsKey(sum)) sumIndex.put(sum, new ArrayList<>());
+
+            sumIndex.get(sum).add(i);
+        }
+
+        return res;
+    }
+
+    public static void main(String[] args) {
+        Solution s = new Solution();
+        int[] nums = {1, 2, 1, 2, 1};
+        System.out.println(s.subarraySum(nums, 3));
+    }
+}
