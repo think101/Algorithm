959a3afa2e3d591c75db2674e8fa807af5473ac9
think101 <taohuayuan2000@hotmail.com>
2023-04-20 23:53:50 -0700

    290. Word Pattern

---
+++ src/main/com/think101/leetcode/Patterns/ArraysHashing/WordPattern/Solution.java
@@ -0,0 +1,32 @@
+package main.com.think101.leetcode.Patterns.ArraysHashing.WordPattern;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class Solution {
+    public boolean wordPattern(String pattern, String s) {
+        Map<Character, String> pToStr = new HashMap<>();
+        String[] strs = s.split(" ");
+
+        if(strs.length != pattern.length()) return false;
+
+        for(int i = 0; i < pattern.length(); i++) {
+            char c = pattern.charAt(i);
+            if(pToStr.containsKey(c)) {
+                if(!pToStr.get(c).equals(strs[i])) return false;
+            }
+            else {
+                pToStr.put(c, strs[i]);
+            }
+        }
+
+        return true;
+    }
+
+    public static void main(String[] args) {
+        Solution s = new Solution();
+        String pattern = "abba";
+        String str = "dog cat cat dog";
+        System.out.println(s.wordPattern(pattern, str));
+    }
+}
