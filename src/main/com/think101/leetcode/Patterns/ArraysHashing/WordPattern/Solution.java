300ade060d6b30817728630f4c2bd4c12f1b8c8b
think101 <taohuayuan2000@hotmail.com>
2023-04-21 13:26:38 -0700

    290. Word Pattern minor improvement

--- src/main/com/think101/leetcode/Patterns/ArraysHashing/WordPattern/Solution.java
+++ src/main/com/think101/leetcode/Patterns/ArraysHashing/WordPattern/Solution.java
@@ -6,20 +6,24 @@ import java.util.Map;
 public class Solution {
     public boolean wordPattern(String pattern, String s) {
         Map<Character, String> pToStr = new HashMap<>();
+        Map<String, Character> strToP = new HashMap<>();
         String[] strs = s.split(" ");
 
         if(strs.length != pattern.length()) return false;
 
         for(int i = 0; i < pattern.length(); i++) {
             char c = pattern.charAt(i);
+
             if(pToStr.containsKey(c)) {
                 if(!pToStr.get(c).equals(strs[i])) return false;
             }
             else {
-                if(pToStr.containsValue(strs[i])) return false;
+                if(strToP.containsKey(strs[i])) return false;
                 pToStr.put(c, strs[i]);
+                strToP.put(strs[i], c);
             }
         }
+
 
         return true;
     }
