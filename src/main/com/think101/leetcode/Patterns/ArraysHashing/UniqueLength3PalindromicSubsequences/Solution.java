78f96b24889054de0f548a973611f88979afd5fb
think101 <taohuayuan2000@hotmail.com>
2023-05-15 23:29:05 -0700

    1930. Unique Length-3 Palindromic Subsequences still TLE solution

--- src/main/com/think101/leetcode/Patterns/ArraysHashing/UniqueLength3PalindromicSubsequences/Solution.java
+++ src/main/com/think101/leetcode/Patterns/ArraysHashing/UniqueLength3PalindromicSubsequences/Solution.java
@@ -5,25 +5,38 @@ import java.util.*;
 public class Solution {
     public int countPalindromicSubsequence(String s) {
         Map<Character, Integer> charCnt = new HashMap<>();
-        List<Set<Character>> positionCharSet = new ArrayList<>();
+        Map<Character, Integer> charCntDup = new HashMap<>();
+
+        // chars after this position
+        List<Set<Character>> afterPosCharSet = new ArrayList<>();
+        // chars before this position
+        List<Set<Character>> beforePosCharSet = new ArrayList<>();
+
+
         Set<String> res = new HashSet<>();
 
         for(int i = 0; i < s.length(); i++) {
             charCnt.put(s.charAt(i), charCnt.getOrDefault(s.charAt(i), 0) + 1);
         }
+        charCntDup = new HashMap<>(charCnt);
 
         for(int i = 0; i < s.length(); i++) {
             char c = s.charAt(i);
             if(charCnt.get(c) == 1) charCnt.remove(c);
             else charCnt.put(c, charCnt.get(c) - 1);
+            afterPosCharSet.add(new HashSet<>(charCnt.keySet()));
 
-            positionCharSet.add(new HashSet<>(charCnt.keySet()));
+            c = s.charAt(s.length() - 1 - i);
+            if(charCntDup.get(c) == 1) charCntDup.remove(c);
+            else charCntDup.put(c, charCntDup.get(c) - 1);
+            beforePosCharSet.add(0, new HashSet<>(charCntDup.keySet()));
         }
 
-        for(int i = 0; i < s.length() - 2; i++) {
-            for(int j = i + 1; j < s.length() - 1; j++) {
-                if(positionCharSet.get(j).contains(s.charAt(i))) {
-                    res.add(String.valueOf(s.charAt(i)) + s.charAt(j) + s.charAt(i));
+        for(int i = 1; i < s.length() - 1; i++) {
+            for(int j = 0; j < 26; j++) {
+                char c = (char)('a' + j);
+                if(beforePosCharSet.get(i).contains(c) && afterPosCharSet.get(i).contains(c)) {
+                    res.add(String.valueOf(c) + s.charAt(i) + c);
                 }
             }
         }
