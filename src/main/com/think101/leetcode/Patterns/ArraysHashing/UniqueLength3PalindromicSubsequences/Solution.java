ab6d294acea705a322850bf64a83490220db65cf
think101 <taohuayuan2000@hotmail.com>
2023-05-16 23:42:25 -0700

    1930. Unique Length-3 Palindromic Subsequences fix TLE

--- src/main/com/think101/leetcode/Patterns/ArraysHashing/UniqueLength3PalindromicSubsequences/Solution.java
+++ src/main/com/think101/leetcode/Patterns/ArraysHashing/UniqueLength3PalindromicSubsequences/Solution.java
@@ -5,42 +5,27 @@ import java.util.*;
 public class Solution {
     public int countPalindromicSubsequence(String s) {
         Map<Character, Integer> charCnt = new HashMap<>();
-        Map<Character, Integer> charCntDup = new HashMap<>();
-
-        // chars after this position
-        List<Set<Character>> afterPosCharSet = new ArrayList<>();
-        // chars before this position
-        List<Set<Character>> beforePosCharSet = new ArrayList<>();
-
 
         Set<String> res = new HashSet<>();
+        Set<Character> left = new HashSet<>();
 
         for(int i = 0; i < s.length(); i++) {
             charCnt.put(s.charAt(i), charCnt.getOrDefault(s.charAt(i), 0) + 1);
         }
-        charCntDup = new HashMap<>(charCnt);
 
-        for(int i = 0; i < s.length(); i++) {
+        for(int i = 0; i < s.length() - 1; i++) {
             char c = s.charAt(i);
             if(charCnt.get(c) == 1) charCnt.remove(c);
             else charCnt.put(c, charCnt.get(c) - 1);
-            afterPosCharSet.add(new HashSet<>(charCnt.keySet()));
 
-            c = s.charAt(s.length() - 1 - i);
-            if(charCntDup.get(c) == 1) charCntDup.remove(c);
-            else charCntDup.put(c, charCntDup.get(c) - 1);
-            beforePosCharSet.add(0, new HashSet<>(charCntDup.keySet()));
-        }
-
-        for(int i = 1; i < s.length() - 1; i++) {
-            Set<Character> bpSet = beforePosCharSet.get(i);
-            Set<Character> apSet = afterPosCharSet.get(i);
             for(int j = 0; j < 26; j++) {
-                char c = (char)('a' + j);
-                if(bpSet.contains(c) && apSet.contains(c)) {
-                    res.add(c + "" + s.charAt(i) + c);
+                char t = (char)('a' + j);
+                if(charCnt.containsKey(t) && left.contains(t)) {
+                    res.add(t + "" + s.charAt(i) + t);
                 }
             }
+
+            left.add(c);
         }
 
         return res.size();
