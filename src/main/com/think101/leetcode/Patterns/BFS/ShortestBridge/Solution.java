f6d2e871bc394ef6a43a8664b5427c737e7d7820
think101 <taohuayuan2000@hotmail.com>
2023-05-21 17:11:30 -0700

    934. Shortest Bridge code beautify

--- src/main/com/think101/leetcode/Patterns/BFS/ShortestBridge/Solution.java
+++ src/main/com/think101/leetcode/Patterns/BFS/ShortestBridge/Solution.java
@@ -6,93 +6,76 @@ import java.util.Queue;
 public class Solution {
     public int shortestBridge(int[][] grid) {
         int n = grid.length;
-        int[][] visited = new int[n][n];
+        int[][] directions = {{-1, 0}, {1, 0}, {0, -1}, {0, 1}};
+        boolean found = false;
 
-        // find all the nodes in 1 island
-        Queue<int[]> nodes = new LinkedList<>();
-        for(int i = 0; i < n; i++) {
-            boolean found = false;
-
-            for(int j = 0; j < n; j++) {
-                if(grid[i][j] == 1) {
-                    visited[i][j] = 1;
-
-                    Queue<int[]> q = new LinkedList<>();
-                    q.add(new int[]{i, j});
-                    while(q.size() > 0) {
-                        int[] node = q.poll();
-
-                        nodes.add(node);
-                        int a = node[0], b = node[1];
-                        if(a - 1 >= 0 && grid[a - 1][b] == 1 && visited[a - 1][b] == 0) {
-                            q.add(new int[]{a - 1, b});
-                            visited[a - 1][b] = 1;
-                        }
-                        if(a + 1 < n && grid[a + 1][b] == 1 && visited[a + 1][b] == 0) {
-                            q.add(new int[]{a + 1, b});
-                            visited[a + 1][b] = 1;
-                        }
-                        if(b - 1 >= 0 && grid[a][b - 1] == 1 && visited[a][b - 1] == 0) {
-                            q.add(new int[]{a, b - 1});
-                            visited[a][b - 1] = 1;
-                        }
-                        if(b + 1 < n && grid[a][b + 1] == 1 && visited[a][b + 1] == 0) {
-                            q.add(new int[]{a, b + 1});
-                            visited[a][b + 1] = 1;
-                        }
-                    }
-
+        // Find the first island and mark all its nodes as visited
+        for (int i = 0; i < n; i++) {
+            if (found) break;
+            for (int j = 0; j < n; j++) {
+                if (grid[i][j] == 1) {
+                    dfs(grid, i, j, directions);
                     found = true;
                     break;
                 }
             }
-
-            if(found) break;
         }
 
-        //visited = new int[n][n];
+        // Traverse the second island using BFS
+        Queue<int[]> queue = new LinkedList<>();
+        boolean[][] visited = new boolean[n][n];
+
+        // Add all the nodes of the first island to the queue
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                if (grid[i][j] == 2) {
+                    queue.add(new int[]{i, j});
+                    visited[i][j] = true;
+                }
+            }
+        }
+
         int steps = 0;
-        while(nodes.size() > 0) {
-            int size = nodes.size();
+        while (!queue.isEmpty()) {
+            int size = queue.size();
+            for (int i = 0; i < size; i++) {
+                int[] node = queue.poll();
+                int x = node[0], y = node[1];
 
-            for(int i = 0; i < size; i++) {
-                int[] node = nodes.poll();
-                int a = node[0], b = node[1];
+                for (int[] direction : directions) {
+                    int nx = x + direction[0];
+                    int ny = y + direction[1];
 
-                if(a - 1 >= 0 && visited[a - 1][b] == 0) {
-                    if(grid[a - 1][b] == 1) return steps;
-                    else{
-                        nodes.add(new int[]{a - 1, b});
-                        visited[a - 1 ][b] = 1;
-                    }
-                }
-                if(a + 1 < n && visited[a + 1][b] == 0) {
-                    if(grid[a + 1][b] == 1) return steps;
-                    else{
-                        nodes.add(new int[]{a + 1, b});
-                        visited[a + 1 ][b] = 1;
-                    }
-                }
-                if(b - 1 >= 0 && visited[a][b - 1] == 0) {
-                    if(grid[a][b - 1] == 1) return steps;
-                    else{
-                        nodes.add(new int[]{a, b - 1});
-                        visited[a][b - 1] = 1;
-                    }
-                }
-                if(b + 1 < n && visited[a][b + 1] == 0) {
-                    if(grid[a][b + 1] == 1) return steps;
-                    else{
-                        nodes.add(new int[]{a, b + 1});
-                        visited[a][b + 1] = 1;
+                    if (nx >= 0 && nx < n && ny >= 0 && ny < n && !visited[nx][ny]) {
+                        if (grid[nx][ny] == 1) {
+                            return steps;
+                        } else {
+                            queue.add(new int[]{nx, ny});
+                            visited[nx][ny] = true;
+                        }
                     }
                 }
             }
-
             steps++;
         }
 
         return steps;
+    }
+
+    // DFS to mark all nodes of the first island as visited
+    private void dfs(int[][] grid, int x, int y, int[][] directions) {
+        int n = grid.length;
+        if (x < 0 || x >= n || y < 0 || y >= n || grid[x][y] != 1) {
+            return;
+        }
+
+        grid[x][y] = 2; // Mark the node as visited
+
+        for (int[] direction : directions) {
+            int nx = x + direction[0];
+            int ny = y + direction[1];
+            dfs(grid, nx, ny, directions);
+        }
     }
 
     public static void main(String[] args) {
