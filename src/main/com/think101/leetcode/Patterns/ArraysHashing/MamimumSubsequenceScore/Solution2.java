9e3c577ad9a102f91b8a307b97e1b53bb5f0e390
think101 <taohuayuan2000@hotmail.com>
2023-05-24 23:24:06 -0700

    2542. Maximum Subsequence Score still wrong

--- src/main/com/think101/leetcode/Patterns/ArraysHashing/MamimumSubsequenceScore/Solution2.java
+++ src/main/com/think101/leetcode/Patterns/ArraysHashing/MamimumSubsequenceScore/Solution2.java
@@ -1,50 +1,50 @@
 package main.com.think101.leetcode.Patterns.ArraysHashing.MamimumSubsequenceScore;
 
+import java.util.Arrays;
 import java.util.Comparator;
 import java.util.PriorityQueue;
 
 public class Solution2 {
     public long maxScore(int[] nums1, int[] nums2, int k) {
-        PriorityQueue<Integer> sortedIndex = new PriorityQueue<>(Comparator.comparingInt(index -> -nums2[index]));
-        PriorityQueue<Integer> minHeap = new PriorityQueue<>(Comparator.comparingInt(index -> -nums1[index]));
+        Integer[] sortedIndex = new Integer[nums2.length];
+        PriorityQueue<Integer> minHeap = new PriorityQueue<>(Comparator.comparingInt(index -> nums1[index]));
 
         for(int i = 0; i < nums2.length; i++) {
-            sortedIndex.add(i);
+            sortedIndex[i] = i;
         }
+
+        Arrays.sort(sortedIndex, Comparator.comparingInt(index -> -nums2[(int) index]));
 
         int sum = 0;
-        int prevIndex = -1;
         for(int i = 0; i < k; i++) {
-            int index = sortedIndex.poll();
+            int index = sortedIndex[i];
             sum += nums1[index];
             minHeap.add(index);
-
-            prevIndex = index;
         }
 
+        int res = sum * nums2[sortedIndex[k - 1]];
 
         for(int i = k; i < nums2.length; i++) {
-            int index = sortedIndex.poll();
+            int index = sortedIndex[i];
 
             int num1MinIndex = minHeap.peek();
 
-            if(sum * nums2[prevIndex] < (sum - nums1[num1MinIndex] + nums1[index]) * nums2[index]) {
+            if(res < (sum - nums1[num1MinIndex] + nums1[index]) * nums2[index]) {
                 sum = sum - nums1[num1MinIndex] + nums1[index];
+                res = sum * nums2[index];
                 minHeap.poll();
                 minHeap.add(index);
             }
-
-            prevIndex = index;
         }
 
-        return sum * nums2[minHeap.peek()];
+        return res;
     }
 
     public static void main(String[] args) {
         Solution2 s = new Solution2();
-        int[] nums1 = {4, 2, 3, 1, 1};
-        int[] nums2 = {7, 5, 10, 9, 6};
-        System.out.println(s.maxScore(nums1, nums2, 1));
+        int[] nums1 = {1, 4};
+        int[] nums2 = {3, 1};
+        System.out.println(s.maxScore(nums1, nums2, 2));
     }
 
 }
