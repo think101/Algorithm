149e106b4c50af686b07507d207fd16bf557d3be
think101 <taohuayuan2000@hotmail.com>
2023-05-28 08:12:47 -0700

    1140. Stone Game II try to resolve TLE

--- src/main/com/think101/leetcode/Patterns/DFS/StoneGameII/Solution.java
+++ src/main/com/think101/leetcode/Patterns/DFS/StoneGameII/Solution.java
@@ -1,6 +1,26 @@
 package main.com.think101.leetcode.Patterns.DFS.StoneGameII;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class Solution {
+    class MemoKey {
+        private int[] piles;
+        private int ind;
+        private int m;
+        private boolean isAlice;
+
+        public MemoKey(int[] piles, int ind, int m, boolean isAlice) {
+            this.piles = piles;
+            this.ind = ind;
+            this.m = m;
+            this.isAlice = isAlice;
+        }
+    }
+
+    private Map<MemoKey, Integer> memo = new HashMap<>();
+
+
     public int stoneGameII(int[] piles) {
         return dfs(piles, 0, 1, true);
     }
@@ -8,6 +28,12 @@ public class Solution {
     private int dfs(int[] piles, int ind, int m, boolean isAlice) {
         if(ind >= piles.length) {
             return 0;
+        }
+
+        MemoKey key = new MemoKey(piles, ind, m, isAlice);
+        if(memo.containsKey(key)) {
+            System.out.println("found in memo");
+            return memo.get(key);
         }
 
         int res = isAlice ? 0 : Integer.MAX_VALUE;
@@ -23,6 +49,8 @@ public class Solution {
             if(isAlice) res = Math.max(res, r + sum);
             else res = Math.min(res, r);
         }
+
+        memo.put(key, res);
 
         return res;
     }
