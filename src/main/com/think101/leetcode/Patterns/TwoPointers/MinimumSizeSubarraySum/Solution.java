3d80c0cd109680ffb240bf2dde80cb1da542ffd9
think101 <taohuayuan2000@hotmail.com>
2023-07-08 21:45:55 -0700

    209. Minimum Size Subarray Sum WA

---
+++ src/main/com/think101/leetcode/Patterns/TwoPointers/MinimumSizeSubarraySum/Solution.java
@@ -0,0 +1,29 @@
+package main.com.think101.leetcode.Patterns.TwoPointers.MinimumSizeSubarraySum;
+
+class Solution {
+    public int minSubArrayLen(int target, int[] nums) {
+        int l = 0, r = 0, sum = nums[0];
+        int res = Integer.MAX_VALUE;
+
+        while(true) {
+            if(sum >= target) res = Math.min(res, r - l + 1);
+            if(r >= nums.length - 1) break;
+
+            while(sum <= target && r < nums.length - 1) sum += nums[++r];
+            if(sum >= target) res = Math.min(res, r - l + 1);
+
+            while(sum >= target && l < r) {
+                sum -= nums[l++];
+                if(sum >= target) res = Math.min(res, r - l + 1);
+            }
+        }
+
+        return res == Integer.MAX_VALUE ? 0 : res;
+    }
+
+    public static void main(String[] args) {
+        Solution s = new Solution();
+        int[] nums = {1, 4, 4 };
+        System.out.println(s.minSubArrayLen(4, nums));
+    }
+}
