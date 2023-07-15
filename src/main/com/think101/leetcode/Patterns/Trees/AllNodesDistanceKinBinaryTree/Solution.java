4fac40b490670f6ee0694aed287935b4839c5dc2
think101 <taohuayuan2000@hotmail.com>
2023-07-14 22:44:54 -0700

    863. All Nodes Distance K in Binary Tree change to use BFS

--- src/main/com/think101/leetcode/Patterns/Trees/AllNodesDistanceKinBinaryTree/Solution.java
+++ src/main/com/think101/leetcode/Patterns/Trees/AllNodesDistanceKinBinaryTree/Solution.java
@@ -2,113 +2,49 @@ package main.com.think101.leetcode.Patterns.Trees.AllNodesDistanceKinBinaryTree;
 
 import main.com.think101.leetcode.Patterns.Trees.TreeNode;
 
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 
 class Solution {
-    //int[] a a[0] is 0 mean node on its left branch, 1 means on its right branch
-    Map<Integer, int[]> preNodes = new HashMap<>();
-    List<Integer> nodes = new ArrayList<>();
-    int originK;
+    Map<Integer, Set<Integer>> neighbors = new HashMap<>();
 
     public List<Integer> distanceK(TreeNode root, TreeNode target, int k) {
-        if(k == 0) {
-            nodes.add(target.val);
-            return nodes;
+        findNeigh(root, null);
+
+        Set<Integer> visited = new HashSet<>();
+
+        Queue<Integer> queue = new LinkedList<>();
+        queue.add(target.val);
+
+        for(int i = 0; i < k; i++) {
+            int size = queue.size();
+            for(int j = 0; j < size; j++) {
+                int elem = queue.poll();
+
+                visited.add(elem);
+                for(int n : neighbors.get(elem)) {
+                    if(!visited.contains(n)) queue.add(n);
+                }
+            }
         }
 
-        if(root.val == target.val) {
-            findChildrenInDistance(root.left, k - 1);
-            findChildrenInDistance(root.right, k - 1);
-            return nodes;
-        }
+        List<Integer> res = new ArrayList<>();
+        res.addAll(queue);
 
-        originK = k;
-        findNode(root, target);
-        findNodesInDistance(root, target, k);
-
-        return nodes;
+        return res;
     }
 
-    // return int[0] 0 means left branch, 1 means right branch
-    private int[] findNode(TreeNode root, TreeNode target) {
-        if(root == null) return new int[]{-1, 0};
-        if(root.val == target.val) return new int[]{1, 0};
+    private void findNeigh(TreeNode node, TreeNode parent) {
+        if(node == null) return;
 
-        int[] res;
+        Set<Integer> ns = new HashSet<>();
+        if(parent != null) ns.add(parent.val);
+        if(node.left != null) ns.add(node.left.val);
+        if(node.right != null) ns.add(node.right.val);
 
-        int[] l = findNode(root.left, target);
+        neighbors.put(node.val, ns);
 
-        if(l != null && l[0] != -1) {
-            res = new int[]{0, l[1] + 1};
-            preNodes.put(root.val, res);
-            return res;
-        }
-
-        int[] r = findNode(root.right, target);
-        if(r != null && r[0] != -1) {
-            res = new int[]{1, r[1] + 1};
-            preNodes.put(root.val, res);
-            return res;
-        }
-
-        return null;
-    }
-
-    private void findNodesInDistance(TreeNode root, TreeNode target, int k) {
-        if(root == null) return;
-
-        if(k == 0) {
-            if(root.val == target.val) {
-                findChildrenInDistance(root.left, originK - 1);
-                findChildrenInDistance(root.right, originK - 1);
-            }
-            else {
-                nodes.add(root.val);
-            }
-
-            return;
-        }
-
-        int[] r = preNodes.get(root.val);
-        if(r == null) return;
-
-        if(r[0] == 0) {
-            // left branch
-            if(r[1] < k) {
-                findNodesInDistance(root.right, target, k - r[1] - 1);
-            }
-            else if(r[1] == originK ) {
-                nodes.add(root.val);
-            }
-
-            findNodesInDistance(root.left, target, r[1] - 1);
-        }
-        else {
-            // right branch
-            if(r[1] < k) {
-                findNodesInDistance(root.left, target, k - r[1] - 1);
-            }
-            else if(r[1] == originK ) {
-                nodes.add(root.val);
-            }
-
-            findNodesInDistance(root.right, target, r[1] - 1);
-        }
-    }
-
-    private void findChildrenInDistance(TreeNode root, int k) {
-        if(k == 0 && root != null) {
-            nodes.add(root.val);
-            return;
-        }
-
-        if(root != null && k > 0) {
-            findChildrenInDistance(root.left, k - 1);
-            findChildrenInDistance(root.right, k - 1);
-        }
+        findNeigh(node.left, node);
+        findNeigh(node.right, node);
     }
 
     public static void main(String[] args) {
