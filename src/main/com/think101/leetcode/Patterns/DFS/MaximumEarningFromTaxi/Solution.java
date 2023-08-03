5442b1b0a3578f985cb3dda5131203d4d4c63f44
think101 <taohuayuan2000@hotmail.com>
2023-08-02 23:24:18 -0700

    2008. Maximum Earnings From Taxi fix TLE

--- src/main/com/think101/leetcode/Patterns/DFS/MaximumEarningFromTaxi/Solution.java
+++ src/main/com/think101/leetcode/Patterns/DFS/MaximumEarningFromTaxi/Solution.java
@@ -1,59 +1,20 @@
 package main.com.think101.leetcode.Patterns.DFS.MaximumEarningFromTaxi;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Set;
-import java.util.TreeMap;
+import java.util.*;
 
 class Solution {
-    TreeMap<Integer, List<int[]>> startToEnds = new TreeMap<>();
-    TreeMap<Integer, Integer> maxEarnAtPos = new TreeMap<>();
-    int res = Integer.MIN_VALUE;
-
-    public long maxTaxiEarnings(int n, int[][] rides) {
-        findStartToEnds(rides);
-
-        Set<Integer> kSet = startToEnds.descendingKeySet();
-        for(Integer key : kSet) {
-            int r = Integer.MIN_VALUE;
-            for(int[] ride : startToEnds.get(key)) {
-                r = Math.max(r, ride[1] - ride[0] + ride[2] + helper(ride[1]));
+    public long maxTaxiEarnings(int n, int[][] A) {
+        Arrays.sort(A, (a, b) -> a[0] - b[0]);
+        long[] dp = new long[n + 1];
+        int j = 0;
+        for(int i = 1; i <= n; ++i) {
+            dp[i] = Math.max(dp[i], dp[i - 1]);
+            while (j < A.length && A[j][0] == i) {
+                dp[A[j][1]] = Math.max(dp[A[j][1]], dp[i] + A[j][1] - A[j][0] + A[j][2]);
+                ++j;
             }
-
-            res = Math.max(res, r);
-            maxEarnAtPos.put(key, r);
         }
-
-        return res;
-    }
-
-    private void findStartToEnds(int[][] rides) {
-        for(int[] ride : rides) {
-            if(!startToEnds.containsKey(ride[0])) {
-                startToEnds.put(ride[0], new ArrayList<>());
-            }
-
-            startToEnds.get(ride[0]).add(ride);
-        }
-    }
-
-    private int helper(int start) {
-        int r = Integer.MIN_VALUE;
-
-        if(maxEarnAtPos.containsKey(start)) {
-            r = maxEarnAtPos.get(start);
-        }
-
-        Integer hKey = maxEarnAtPos.higherKey(start);
-        while(hKey != null) {
-            r = Math.max(r, maxEarnAtPos.get(hKey));
-            hKey = maxEarnAtPos.higherKey(hKey);
-        }
-
-        r = r == Integer.MIN_VALUE ? 0 : r;
-        maxEarnAtPos.put(start, r);
-
-        return r;
+        return dp[n];
     }
 
     public static void main(String[] args) {
